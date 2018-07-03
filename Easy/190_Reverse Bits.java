237addc09acb2573a5f722e93a594df060d639e1
CTCbaxia <ctcbaxia@gmail.com>
2018-07-03 21:05:02 +0800

    E0-7.3
    72.6%

---
+++ Easy/190_Reverse Bits.java
@@ -0,0 +1,25 @@
+/*
+190. Reverse Bits
+https://leetcode.com/problems/reverse-bits/description/
+
+
+*/
+
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int result = 0;
+        int lastnum = 0;
+
+        for( int i = 0; i < 32; i ++){
+
+            lastnum = n & 1;
+            n >>> 1;
+
+            result = result << 1;
+            result = result | lastnum;
+            
+        }
+
+    }
+}
