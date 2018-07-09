128cee660922d3ec7d66bad331fc5d69ae4bb476
CTCbaxia <ctcbaxia@gmail.com>
2018-07-09 16:40:21 +0800

    0709

--- Easy/35.java
+++ Easy/35.java
@@ -3,16 +3,20 @@
 https://leetcode.com/problems/search-insert-position/description/
 
 TIME: 0708
-RESULT: 
+RESULT: 100%, 3ms
+NOTE: 注意边界条件（nums == []）
 */
 
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        if(nums[0] > target){
+        if(nums.length == 0){
             return 0;
-        }else if(nums[nums.length - 1] == target){
+        }else if(nums[0] > target){
+            return 0;
+        }else if(nums[nums.length - 1] < target){
             return nums.length;
         }
+        
         int position_left = 0;
         for(int i = 0; i < nums.length; i++){
             if(nums[i] == target){
