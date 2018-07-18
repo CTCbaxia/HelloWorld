5149bc9f641cdcfcdc19fd594dfd0b6d4fe47435
CTCbaxia <ctcbaxia@gmail.com>
2018-07-18 14:19:21 +0800

    0718 - 387. First Unique Character in a String

---
+++ Easy/387-First-Unique Character-in-a-String.java
@@ -0,0 +1,56 @@
+/*
+387. First Unique Character in a String
+https://leetcode.com/problems/first-unique-character-in-a-string/description/
+
+TIME: 0718 - 30min
+RESULT: 38.61% - 58ms
+NOTES: 
+对于单词每个 char 的频率，用 char[] 存储比用 hashmap 存储更快。
+
+METHOD:
+1. HashMap
+2. char[]
+
+*/
+
+/*
+SOLUTION 0:
+
+TIME: 0718 - 5min
+RESULT: 88% - 17ms
+*/
+class Solution {
+    public int firstUniqChar(String s) {
+        char[] charfreq = new char[26];
+        for(int i = 0; i < s.length(); i++){
+            charfreq[s.charAt(i) - 'a'] ++;
+        }
+        for(int i = 0; i < s.length(); i++){
+            if(charfreq[s.charAt(i) - 'a'] == 1) return i;
+        }
+        return -1;
+    }
+}
+
+
+
+/*
+SOLUTION 1:
+
+TIME: 0718 - 30min
+RESULT: 38.61% - 58ms
+*/
+class Solution {
+    public int firstUniqChar(String s) {
+        Map<Character, Integer> charmap = new HashMap<Character, Integer>();
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            charmap.put(c,charmap.getOrDefault(c, 0) + 1);
+        }
+        for(int i = 0; i < s.length(); i++){
+            if(charmap.get(s.charAt(i)) > 1) continue;
+            else return i; 
+        }
+        return -1;
+    }
+}
