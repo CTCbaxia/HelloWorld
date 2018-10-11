a7d1a5d56b1aa999fec39bf7213678e0e5f0df92
CTCbaxia <ctcbaxia@gmail.com>
2018-10-11 10:48:31 -0400

    1011

--- Easy/344-Reverse-String.java
+++ Easy/344-Reverse-String.java
@@ -4,10 +4,50 @@ EASY
 https://leetcode.com/problems/reverse-string/description/
 
 TIME: 0929 - 2min
-RESULT: 44% - 3ms
+RESULT: 100% - 1ms
 NOTES: 
 
 */
+//char[] swap
+class Solution {
+    public String reverseString(String s) {
+        char[] c = s.toCharArray();
+        for(int i = 0; i < c.length/2; i++){
+            char tmp = c[i];
+            c[i] = c[c.length - 1 - i];
+            c[c.length - 1 - i] = tmp;
+        }
+        return String.valueOf(c);
+    }
+}
+
+
+class Solution {
+    public String reverseString(String s) {
+        char[] c = s.toCharArray();
+        int i = 0;
+        int j = c.length - 1;
+        while(i < j){
+            char tmp = c[i];
+            c[i] = c[j];
+            c[j] = tmp;
+            i++;
+            j--;
+        }
+        return new String(c);
+    }
+}
+
+//StringBuilder to build another
+
+class Solution {
+    public String reverseString(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        return sb.reverse().toString();
+    }
+}
+
+
 class Solution {
     public String reverseString(String s) {
         StringBuilder sb = new StringBuilder();
