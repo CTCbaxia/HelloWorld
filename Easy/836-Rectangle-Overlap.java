1af5a77d1cc0ce022eee23cc6d6af872e19f6981
CTCbaxia <ctcbaxia@gmail.com>
2018-10-11 16:19:42 -0400

    1011 -836. Rectangle Overlap

---
+++ Easy/836-Rectangle-Overlap.java
@@ -0,0 +1,30 @@
+/*
+EASY
+836. Rectangle Overlap
+
+
+TIME: 1011 - 
+RESULT:
+NOTES: 
+
+*/
+//如果给恶心的数字会overflow
+class Solution {
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+        return isOverlap(rec1[0], rec1[2], rec2[0], rec2[2]) && isOverlap(rec1[1], rec1[3], rec2[1], rec2[3]);
+    }
+    private boolean isOverlap(int a1, int a2, int b1, int b2){
+        int min = Math.min(a1, b1);
+        int max = Math.max(a2, b2);
+        if(max - min < (a2 - a1) + (b2 - b1)) return true;
+        else return false;
+    }
+}
+
+//想不可能的情况更简单
+class Solution {
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+        return (!(rec1[2] <= rec2[0] || rec1[3] <= rec2[1] || rec2[2] <= rec1[0] || rec2[3] <= rec1[1]));
+    }
+
+}
