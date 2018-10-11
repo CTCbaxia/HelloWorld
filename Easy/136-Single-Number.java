ce5a9e3f3e278a89c6f675a90547c20f7a5d4e83
CTCbaxia <ctcbaxia@gmail.com>
2018-10-11 13:42:05 -0400

    1011

--- Easy/136-Single-Number.java
+++ Easy/136-Single-Number.java
@@ -15,6 +15,22 @@ METHOD:
 
     
 */
+//--------2 ROUND FOR MS ------------------------------------------------
+class Solution {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for(int n : nums){
+            result ^= n;
+        }
+        return result;
+    }
+}
+
+
+
+
+//--------1 ROUND ------------------------------------------------
+
 /*
 SOLUTION REFERENCE:
 
