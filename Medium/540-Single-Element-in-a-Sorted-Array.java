53664977681476163e522da9ee0405c14c0ef6eb
CTCbaxia <ctcbaxia@gmail.com>
2018-10-11 16:19:26 -0400

    26. Remove Duplicates from Sorted Array

--- Medium/540-Single-Element-in-a-Sorted-Array.java
+++ Medium/540-Single-Element-in-a-Sorted-Array.java
@@ -14,7 +14,29 @@ THOUGHT:
 如果 N - 1 / 2 为奇数个，那么中间数的对象应该在它左边（if so, 继续查右边数组） -> 如果在右边，那么只需继续查询左边的数组
 如果 N - 1 / 2 为偶数个，那么中间数的对象应该在它右边（if so, 继续查右边数组） -> 如果在左边，那么只需继续查询左边的数组
 */
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int lo = 0;
+        int hi = nums.length - 1;
+        
+        while(lo < hi){
+            System.out.println(lo + " "+hi);
+            int len = (hi - lo) / 2;
+            int mid = lo + len;
+            if(len % 2 == 0){
+                if(nums[mid] == nums[mid - 1]) hi = mid - 2;
+                else if(nums[mid] == nums[mid + 1]) lo = mid + 2;
+                else return nums[mid];                
+            }else{
+                if(nums[mid] == nums[mid - 1]) lo = mid + 1;
+                else if(nums[mid] == nums[mid + 1]) hi = mid - 1;
+                else return nums[mid];   
+            }
 
+        }
+        return nums[lo];
+    }
+}
 /*
 TIME: 0806 - 30min
 RESULT: 25% - 1ms
