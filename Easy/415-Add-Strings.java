695c2bd2fb5be3db33a59fcc6f75cd9fd7de2e6e
CTCbaxia <ctcbaxia@gmail.com>
2018-10-10 22:23:50 -0400

    1010

--- Easy/415-Add-Strings.java
+++ Easy/415-Add-Strings.java
@@ -10,6 +10,29 @@ NOTES:
 RELATED:
 1. 43. Multiply Strings
 */
+
+class Solution {
+    public String addStrings(String num1, String num2) {
+        char[] n1 = num1.toCharArray();
+        char[] n2 = num2.toCharArray();
+        int i = n1.length - 1; 
+        int j = n2.length - 1;
+        int carry = 0;
+        StringBuilder sb = new StringBuilder();
+        while(i >= 0 || j >= 0){
+            int digit1 = (i < 0) ? 0 : n1[i] - '0';
+            int digit2 = (j < 0) ? 0 : n2[j] - '0';
+            int sum = digit1 + digit2 + carry;
+            carry = sum / 10;
+            sb.append(sum = sum % 10);
+            i--;
+            j--;
+        }
+        if(carry != 0) sb.append(carry);
+        return sb.reverse().toString();
+    }
+}
+
 /*
 SOLUTION 0:
 TIME: 0823 - 30min
