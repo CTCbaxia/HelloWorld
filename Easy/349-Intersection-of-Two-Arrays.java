94b78f75fdb5163307e2cb4ad1876fee179eeacc
CTCbaxia <ctcbaxia@gmail.com>
2018-10-28 15:38:19 -0400

    1028 - 349

--- Easy/349-Intersection-of-Two-Arrays.java
+++ Easy/349-Intersection-of-Two-Arrays.java
@@ -8,6 +8,77 @@ RESULT: 90% - 3ms
 
 */
 /*
+sort + two pointers
+
+Time: O(nlogn)
+Space: O(1)
+*/
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        if(nums1.length == 0 || nums2.length == 0) return new int[0];
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0;
+        int j = 0;
+        List<Integer> list = new ArrayList<Integer>();
+        while(i < nums1.length && j < nums2.length){
+            if(nums1[i] < nums2[j]) i++;
+            else if(nums1[i] > nums2[j]) j++;
+            else{
+                list.add(nums1[i]);
+                i++;
+                j++;
+                while(i < nums1.length && nums1[i] == nums1[i - 1]) i++;
+                while(j < nums2.length && nums2[j] == nums2[j - 1]) j++;
+            }
+        }
+        int[] result = new int[list.size()];
+        for(int k = 0; k < result.length; k++){
+            result[k] = list.get(k);
+        }
+        return result;
+    }
+}
+
+
+
+/*
+hashset + two for loop
+**如果一个很小 一个很大，用很小的搭 set 可以省空间
+Time: O(m + n)
+Space: O(n)
+*/
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<Integer>();
+        Set<Integer> intersect = new HashSet<Integer>();
+        for(int i : nums1) set.add(i);
+        for(int j : nums2){
+            if(set.contains(j)) intersect.add(j);
+        }
+        
+        int[] result = new int[intersect.size()];
+        int index = 0;
+        for(int n : intersect){
+            result[index++] = n;
+        }
+        return result;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 SOLUTION 1: Sort
 TIME: 0907 - 30min
 RESULT: 90% - 3ms
