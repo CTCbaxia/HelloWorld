9d1c1fb28491456fa68d0c44f8b94bfc946345f8
CTCbaxia <ctcbaxia@gmail.com>
2018-10-29 21:26:10 -0400

    1029 - 33

--- Medium/33-Search-in-Rotated-Sorted-Array.java
+++ Medium/33-Search-in-Rotated-Sorted-Array.java
@@ -10,6 +10,73 @@ NOTES:
 */
 class Solution {
     public int search(int[] nums, int target) {
+        int lo = 0; 
+        int hi = nums.length - 1;
+        while(lo <= hi){//compare all suitable elements in nums
+            int mid = lo + (hi - lo)/2;
+            if(nums[mid] == target) return mid;
+            
+            if(nums[hi] > nums[mid]){//right good, increasing
+                if(target > nums[mid] && target <= nums[hi]) lo = mid + 1; //如果 target 落在 right 的范围（因为右边的大小有序）
+                else hi = mid - 1;
+            }else{//left good, increasing
+                if(target >= nums[lo] && target < nums[mid]) hi = mid - 1;//如果 target 落在 left 的范围（因为左边的大小有序）
+                else lo = mid + 1;
+            }
+        }
+        //lo != hi but must be not match in nums
+        return -1;
+    }
+
+}
+/*
+先判断到底那边是线性完全 increasing 的(condition A or B)
+然后判断 target 在不在那一半里
+       /|
+     A/ | 
+     /  |   hi
+    lo  |  /
+        | /B
+        |/
+
+*/
+
+
+
+
+class Solution {
+    public int search(int[] nums, int target) {
+        if(nums.length == 0) return -1;
+        int lo = 0; 
+        int hi = nums.length - 1;
+        while(lo < hi){
+            int mid = lo + (hi - lo)/2;
+            if(nums[hi] > nums[mid]){//right good
+                if(nums[mid] > target) hi = mid;
+                else if(nums[mid] < target){
+                    if(nums[hi] < target) hi = mid;
+                    else lo = mid + 1;
+                }else{
+                    return mid;
+                }
+            }else{//left good
+                if(nums[mid] > target){
+                    if(nums[lo] > target) lo = mid + 1;
+                    else hi = mid;
+                }else if(nums[mid] < target) lo = mid + 1;
+                else{
+                    return mid;
+                }
+            }
+        }
+        //lo == hi
+        return (nums[hi] == target) ? hi : -1;
+    }
+
+}
+
+class Solution {
+    public int search(int[] nums, int target) {
         if(nums.length == 0) return -1;
         int left = 0;
         int right = nums.length - 1;
