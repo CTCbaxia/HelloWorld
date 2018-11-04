416966900c5c71948c25c92637aef4e3c27330b2
CTCbaxia <ctcbaxia@gmail.com>
2018-11-03 21:10:21 -0400

    1103 - 694. Number of Distinct Islands

---
+++ Medium/694-Number-of-Distinct-Islands.java
@@ -0,0 +1,60 @@
+/*
+MEDIUM
+694. Number of Distinct Islands
+
+TIME: 
+RESULT: 
+NOTES:
+
+*/
+/*
+DFS
+We just need the relative position to the original point for an island
+So we use StringBuilder to build the island parts
+
+
+Time: O(mn)? main part is the loop of the grid
+Space: O(mn) ? worst case: all 1
+注意这题 offset 的方法不行，要用（i，j) 双坐标定位，下面的也会成立
+0 1 1
+1 1 1
+0 0 0
+1 1 1
+1 1 0
+*/
+class Solution {
+    public int numDistinctIslands(int[][] grid) {
+        if(grid.length == 0 || grid[0].length == 0) return 0;
+        
+        Set<String> result = new HashSet<String>();
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
+                if(grid[i][j] == 1){
+                    StringBuilder sb = new StringBuilder();
+                    find(grid, i, j, i, j, sb);
+                    String s = sb.toString();
+                    if(result.contains(s)) continue;
+                    else result.add(s);
+                }
+            }
+        }
+        return result.size();
+    }
+    private void find(int[][] grid, int i, int j, int oi, int oj, StringBuilder sb){
+        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length) return;
+        
+        if(grid[i][j] == 1){
+            sb.append(i - oi).append(j - oj);
+            grid[i][j] = 0;
+        }else return;
+        
+        int[][] moves = {{1, 0},{-1, 0}, {0, 1},{0, -1}};
+        for(int[] move : moves){
+            find(grid, i + move[0], j + move[1], oi, oj, sb);
+        }
+        return;
+    }
+}
+
+
+
