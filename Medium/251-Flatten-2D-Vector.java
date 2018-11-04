ea9945906f750b042e11eae5167074b7d3dc3f61
CTCbaxia <ctcbaxia@gmail.com>
2018-11-03 22:08:00 -0400

    1103 - 251. Flatten 2D Vector

---
+++ Medium/251-Flatten-2D-Vector.java
@@ -0,0 +1,76 @@
+/*
+MEDIUM
+251. Flatten 2D Vector
+
+TIME: 
+RESULT: 
+NOTES:
+
+*/
+
+/*
+only use hasNext + next 
+
+Time: O(1)
+Space: O(1)
+*/
+public class Vector2D implements Iterator<Integer> {
+    Iterator<List<Integer>> list;
+    Iterator<Integer> it;
+    public Vector2D(List<List<Integer>> vec2d) {
+        //if(vec2d == null) return;
+        this.list = vec2d.iterator();
+    }
+
+    @Override
+    public Integer next() {
+        return it.next();
+    }
+
+    @Override
+    public boolean hasNext() {
+        while((it == null || !it.hasNext()) && list.hasNext()){
+            it = list.next().iterator();
+        }
+        return it != null && it.hasNext();
+    }
+}
+
+/**
+ * Your Vector2D object will be instantiated and called as such:
+ * Vector2D i = new Vector2D(vec2d);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
+
+public class Vector2D implements Iterator<Integer> {
+    int i, j;
+    int len;
+    List<List<Integer>> input;
+    public Vector2D(List<List<Integer>> vec2d) {
+        this.i = 0;
+        this.j = 0;
+        input = vec2d;
+    }
+
+    @Override
+    public Integer next() {
+        return input.get(i).get(j++);
+    }
+
+    @Override
+    public boolean hasNext() {
+        if(i >= input.size()) return false;
+        else if(j < input.get(i).size()) return true;
+        else{
+            j = 0; 
+            i++;
+            return hasNext();
+        }
+    }
+}
+
+/**
+ * Your Vector2D object will be instantiated and called as such:
+ * Vector2D i = new Vector2D(vec2d);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
