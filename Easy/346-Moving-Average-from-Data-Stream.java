2a3b2519e694253e47427885eaf3aac5e4ebc18e
CTCbaxia <ctcbaxia@gmail.com>
2018-11-03 20:05:16 -0400

    1103 - 346. Moving Average from Data Stream

---
+++ Easy/346-Moving-Average-from-Data-Stream.java
@@ -0,0 +1,63 @@
+/*
+EASY
+346. Moving Average from Data Stream
+
+RESULT: 
+NOTES: 
+*/
+/*
+Window + pointer
+data: the window of sum
+index: inserted index
+sum: 
+n: divisor (1, 2, ..., size)
+*/
+class MovingAverage {
+    int[] data;
+    int index, n;
+    int sum;
+    /** Initialize your data structure here. */
+    public MovingAverage(int size) {
+        this.data = new int[size];
+        index = 0;
+        n = 0;
+        sum = 0;
+    }
+    
+    public double next(int val) {
+        if(n < data.length) n++;
+        sum += val;
+        sum -= data[index];
+        data[index] = val;
+        index = (index + 1) % data.length;
+        return (double) sum / n;
+    }
+}
+
+class MovingAverage {
+    Queue<Integer> q;
+    int size;
+    /** Initialize your data structure here. */
+    public MovingAverage(int size) {
+        this.q = new LinkedList<Integer>();
+        this.size = size;
+    }
+    
+    public double next(int val) {
+        q.offer(val);
+        if(q.size() > size) q.poll();
+        Iterator<Integer> it = q.iterator();
+        
+        double sum = 0;
+        while(it.hasNext()){
+            sum += it.next();
+        }
+        return sum / q.size();
+    }
+}
+
+/**
+ * Your MovingAverage object will be instantiated and called as such:
+ * MovingAverage obj = new MovingAverage(size);
+ * double param_1 = obj.next(val);
+ */
