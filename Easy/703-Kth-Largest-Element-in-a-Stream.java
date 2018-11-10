7dba4d7f1b5dea3a236b3adfcf8259d867b26832
CTCbaxia <ctcbaxia@gmail.com>
2018-11-10 10:32:56 -0500

    1110 - 703. Kth Largest Element in a Stream

---
+++ Easy/703-Kth-Largest-Element-in-a-Stream.java
@@ -0,0 +1,37 @@
+/*
+EASY
+703. Kth Largest Element in a Stream
+
+RESULT: 
+NOTES: 
+*/
+/*
+PriorityQueue: 
+
+Time: O(nlogn)
+Space: O(1)
+*/
+class KthLargest {
+    public int k;
+    public PriorityQueue<Integer> pq;
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        pq = new PriorityQueue<Integer>();
+        for(int i = 0; i < nums.length; i++){
+            pq.offer(nums[i]);
+            if(pq.size() > k) pq.poll();
+        }
+    }
+    
+    public int add(int val) {
+        pq.offer(val);
+        if(pq.size() > k) pq.poll();
+        return pq.peek();
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
