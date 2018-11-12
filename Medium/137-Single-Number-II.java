b7581781e90671dba38dc5557717ac6892093454
CTCbaxia <ctcbaxia@gmail.com>
2018-11-12 17:34:14 -0500

    1112 - 137. Single Number II

---
+++ Medium/137-Single-Number-II.java
@@ -0,0 +1,67 @@
+/*
+MEDIUM
+137. Single Number II
+
+TIME: 
+RESULT: 
+NOTES:
+
+*/
+/*
+Bit Calculation
+for 32 bit, check every bit if that bit is 1 in the single number
+
+Time: O(n)
+Space: O(1)
+*/
+class Solution {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for(int i = 0; i < 32; i++){
+            int sum = 0;//sum how many 1 in that bit
+            for(int k = 0; k < nums.length; k++){
+                if((nums[k] >> i & 1) == 1){
+                    sum++;
+                }
+            }
+            sum = sum % 3;
+            if(sum == 1) result += 1 << i;
+        }
+        return result;
+    }
+}
+
+
+
+
+
+/*
+Bit Manipulation:
+
+set ^ num -- add num to set
+
+"(ones ^ A[i]) & ~twos" basically means perform the above mentioned operation if and only if A[i] is not present in the set "twos". So to write it in layman:
+
+   IF the set "ones" does not have A[i]
+       Add A[i] to the set "ones" if and only if its not there in set "twos"
+   ELSE
+       Remove it from the set "ones"
+
+one: number shows only exactly once
+two: number shows only exactly two
+
+
+
+Time: O(n)
+Space: O(1)
+*/
+class Solution {
+    public int singleNumber(int[] nums) {
+        int one = 0, two = 0;
+        for(int i = 0; i < nums.length; i++){
+            one = (one ^ nums[i]) & ~two;//add nums[i] to one group only if it is not in two
+            two = (two ^ nums[i]) & ~one;//add nums[i] to two group only if it is not in one
+        }
+        return one;
+    }
+}
