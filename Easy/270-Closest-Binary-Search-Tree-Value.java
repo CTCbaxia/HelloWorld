b24a2ecf667839bcc174371f1b84f6a8edcc4ebe
CTCbaxia <ctcbaxia@gmail.com>
2018-11-17 21:26:33 -0500

    1117 - 270

--- Easy/270-Closest-Binary-Search-Tree-Value.java
+++ Easy/270-Closest-Binary-Search-Tree-Value.java
@@ -7,7 +7,6 @@ NOTES:
 */
 /*
 BST
-
 For every node we pass, store the min difference and the corresponding node
 
 Time: O(logn)
@@ -16,15 +15,15 @@ Space: O(1)
 class Solution {
     public int closestValue(TreeNode root, double target) {
         int res = root.val;
-        double min = Double.MAX_VALUE;//remember to use Double.MAX_VALUE
+        double diff = Double.MAX_VALUE;//remember to use Double.MAX_VALUE
         
         while(root != null){
-            if(min > Math.abs(target - (double)root.val)){
-                min = Math.abs(target - (double)root.val);
+            double d = Math.abs(root.val - target);
+            if(d < diff){
+                diff = d;
                 res = root.val;
             }
-            
-            if(target > root.val) root = root.right;
+            if(root.val < target) root = root.right;
             else root = root.left;
         }
         return res;
