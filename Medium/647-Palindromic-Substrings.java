fede27d0eaf5cc56112c887641a01af43ed6c3b1
CTCbaxia <ctcbaxia@gmail.com>
2018-11-18 16:41:01 -0500

    1118 - 647

--- Medium/647-Palindromic-Substrings.java
+++ Medium/647-Palindromic-Substrings.java
@@ -8,7 +8,9 @@ NOTES:
 
 */
 /*
-odd and even extend check:
+Two Pointers: Center extend
+
+Odd and even extend check
 for every element i, 
 count palindrom substrings with i as center
 count palindrom substrings with i, i + 1 as center
@@ -18,20 +20,19 @@ Space: O(1)
 */
 class Solution {
     public int countSubstrings(String s) {
-        int count = 0;
-        for(int i = 0; i < s.length(); i++){
-            count += palindrom(s, i, i); //odd palindrom with i as center
-            count += palindrom(s, i, i + 1); //even palindrom with i, i+1 as center
-        }
-        return count;
-    }
-    private int palindrom(String s, int left, int right){//find palindrom with left and right as center
         int res = 0;
-        while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
-            res++;
-            left--;
-            right++;
+        for(int i = 0; i < s.length(); i++){
+            res += count(s, i, i);//odd palindrom with i as center
+            res += count(s, i, i + 1);//even palindrom with i, i+1 as center
         }
         return res;
+    }
+    private int count(String s, int left, int right){//find palindrom with left and right as center
+        int count = 0;
+        while(left >= 0 && right < s.length()){
+            if(s.charAt(left--) == s.charAt(right++)) count++;
+            else return count;
+        }
+        return count;
     }
 }
