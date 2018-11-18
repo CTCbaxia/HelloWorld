a89df07a6b9af7e9ac4fe17fb070233784e6e794
CTCbaxia <ctcbaxia@gmail.com>
2018-11-17 22:13:12 -0500

    1117 - 100

--- Easy/100-Same-Tree.java
+++ Easy/100-Same-Tree.java
@@ -8,6 +8,21 @@ RESULT: 20% - 4ms
 NOTES: 
 高票满分回答没区别
 */
+/*
+recursive
+
+Time: O(n)
+Space: O(1)
+*/
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null) return q == null;
+        if(p.val != q.val) return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
+
+
 
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
