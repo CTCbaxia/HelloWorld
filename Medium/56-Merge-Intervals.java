ccf639cff6470e0676108f65eb83f0adcaf3500c
CTCbaxia <ctcbaxia@gmail.com>
2018-11-19 10:07:41 -0500

    1119 - 56

--- Medium/56-Merge-Intervals.java
+++ Medium/56-Merge-Intervals.java
@@ -13,6 +13,55 @@ METHOD:
 先将所有 intervals 排序。
 */
 
+/*
+Sort by start
+manage a tmp, 
+when overlap, chosing the larger end as the tmp.end
+when not, add tmp to result, update the new tmp
+after loop, add the last tmp to result
+
+Time: O(nlogn)
+Space: O(1)
+*/
+class Solution {
+    public List<Interval> merge(List<Interval> intervals) {
+        List<Interval> result = new ArrayList<>();
+        if(intervals.size() == 0) return result;
+        
+        Collections.sort(intervals, new Comparator<Interval>(){
+            public int compare(Interval i1, Interval i2){
+                return i1.start - i2.start;
+            }
+        });
+        
+        Interval tmp = intervals.get(0);
+        for(int i = 1; i < intervals.size(); i++){
+            if(intervals.get(i).start <= tmp.end){
+                tmp.end = Math.max(intervals.get(i).end, tmp.end);
+            }else{
+                result.add(tmp);
+                tmp = intervals.get(i);
+            }
+        }
+        result.add(tmp);
+        return result;
+    }
+}
+
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+
+
+
+
+
 
 
 class Solution {
