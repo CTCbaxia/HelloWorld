3bfe5049e5b0ea0faf54d1de5a03ce5c448ad35a
CTCbaxia <ctcbaxia@gmail.com>
2018-11-19 10:42:20 -0500

    1119 - 554

--- Medium/554-Brick-Wall.java
+++ Medium/554-Brick-Wall.java
@@ -9,6 +9,33 @@ NOTES:
 */
 /*
 Map + update count result
+Map: <end point, frequence>
+result: everytime you update the map, also update the result if get larger count
+
+Time: O(n)
+Space: O(n)
+*/
+    class Solution {
+    public int leastBricks(List<List<Integer>> wall) {
+        int res = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for(List<Integer> level : wall){//需要声明 type
+            int end = 0;
+            for(int i = 0; i < level.size() - 1; i++){
+                end += level.get(i);
+                map.put(end, map.getOrDefault(end, 0) + 1);
+                res = Math.max(res, map.get(end));
+            }
+        }
+        return wall.size() - res;//需要穿过几道墙
+    }
+}
+
+
+
+
+/*same as above
+Map + update count result
 Map: <cross line index, count of rows that has that cross line>
 result: everytime you update the map, also update the result if get larger count
 
