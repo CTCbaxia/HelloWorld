2316bc760f11e7b31081a3f0d4acc06f7f1a11b5
CTCbaxia <ctcbaxia@gmail.com>
2018-11-21 00:52:31 -0500

    1120 - 238

--- Medium/238-Product-of-Array-Except-Self.java
+++ Medium/238-Product-of-Array-Except-Self.java
@@ -8,6 +8,39 @@ NOTES:
 
 */
 /*
+双向计算
+
+Time: O(n)
+Space: O(n)
+*/
+class Solution {
+    public int[] productExceptSelf(int[] nums) {//[1,2,3,4]
+        int[] left = new int[nums.length];
+        int[] right = new int[nums.length];
+        int[] result = new int[nums.length];
+        
+        int product = 1;
+        for(int i = 0; i < nums.length; i++){
+            left[i] = product;
+            product *= nums[i];
+        }//[1,1,2,6]
+        
+        product = 1;
+        for(int i = nums.length - 1; i >= 0; i--){
+            right[i] = product;//如果这直接 result[i] *= product，就是 in-place 了
+            product *= nums[i];
+        }//[24,12,4,1]
+        
+        for(int i = 0; i < nums.length; i++){
+            result[i] = left[i] * right[i];
+        }
+        return result;
+    }
+}
+
+
+
+/*
 DP: save the result + two loop
 Time: O(n)
 Space: O(1)
