dac5927c53c538097e8054aec6e993889181bedb
CTCbaxia <ctcbaxia@gmail.com>
2018-11-20 22:16:18 -0500

    1120 - 278

--- Easy/278-First-Bad-Version.java
+++ Easy/278-First-Bad-Version.java
@@ -13,16 +13,17 @@ NOTES:
 Time: O(logn)
 Space: O(1)
 */
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        int lo = 0; 
-        int hi = n;
+        int lo = 1, hi = n;
         while(lo < hi){
-            int mid = lo + (hi - lo) / 2;
+            int mid = lo + (hi - lo)/2;
             if(isBadVersion(mid)) hi = mid;
             else lo = mid + 1;
         }
-        return hi;
+        return lo;
     }
 }
