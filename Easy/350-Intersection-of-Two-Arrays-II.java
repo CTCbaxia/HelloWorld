41eef75083483a13c6552afe7bc0e79c00b7e7ab
CTCbaxia <ctcbaxia@gmail.com>
2018-11-22 12:01:09 -0500

    1122 - 350

--- Easy/350-Intersection-of-Two-Arrays-II.java
+++ Easy/350-Intersection-of-Two-Arrays-II.java
@@ -15,7 +15,7 @@ FOLLOW UP:
 - Space: O(m)
 
 3. What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
-???
+
 I think the goal of this question is to test whether the interview understands some of the data engineering techniques. From a data engineer's perspective, basically there are three ideas to solve the question:
 1. Store the two strings in distributed system(whether self designed or not), then using MapReduce technique to solve the problem;
 2. Processing the Strings by chunk, which fits the memory, then deal with each chunk of data at a time;
@@ -56,30 +56,8 @@ class Solution {
 }
 
 
-//more complicated one
-class Solution {
-    public int[] intersect(int[] nums1, int[] nums2) {
-        if(nums1.length == 0 || nums2.length == 0) return new int[0];
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();//<key, number of key>
-        for(int i = 0; i < nums1.length; i++){
-            map.put(nums1[i], map.getOrDefault(nums1[i],0) + 1);
-        }
-        List<Integer> l = new ArrayList<Integer>();
-        for(int i = 0; i < nums2.length; i++){
-            if(map.containsKey(nums2[i])){
-                l.add(nums2[i]);
-                if(map.get(nums2[i]) - 1 == 0) map.remove(nums2[i]);
-                else map.put(nums2[i], map.get(nums2[i]) - 1);
-            }
-        }
-        
-        int[] result = new int[l.size()];
-        for(int i = 0; i < l.size(); i++){
-            result[i] = l.get(i);
-        }
-        return result;
-    }
-}
+
+
 
 
 /*
