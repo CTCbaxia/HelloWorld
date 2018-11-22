9924437bd559f3c8fddd116a2f00c909f770b0d5
CTCbaxia <ctcbaxia@gmail.com>
2018-11-21 22:37:51 -0500

    1121 - 277

--- Medium/277-Find-the-Celebrity.java
+++ Medium/277-Find-the-Celebrity.java
@@ -7,6 +7,52 @@ RESULT: 75% - 10ms
 NOTES:
 如果要求尽量少的调用 API, 就注意最大化利用 API 信息的不同结果
 */
+/*
+调用一次knows(a, b)就要尽可能多得到信息
+knows(a, b) can tell you who cannot be the celebrity
+*/
+/* The knows API is defined in the parent class Relation.
+      boolean knows(int a, int b); */
+
+public class Solution extends Relation {
+    public int findCelebrity(int n) {
+        int pending = 0;
+        for(int i = 1; i < n; i++){
+             if(knows(pending, i)){
+                 pending = i;
+             }
+        }
+        //check if pending is the real celebrity：是否满足双向关系
+        //knows(pending, i) 其实可以只查 pending前面的，因为后面的都在上面查过了
+        for(int i = 0; i < n; i++){
+            if(i == pending) continue;
+            if(knows(pending, i) || !knows(i, pending)) return  -1;
+        }
+        return pending;
+    }
+}
+
+//更优化的解法（调用 knows(a,b)更少）
+public class Solution extends Relation {
+    public int findCelebrity(int n) {
+        int pending = 0;
+        for(int i = 1; i < n; i++){
+             if(knows(pending, i)){
+                 pending = i;
+             }
+        }
+        for(int i = 0; i < pending; i++){//pending 之后的都在上面的loop里面查过
+            if(knows(pending, i)) return -1;
+        }
+        //check if pending is the real celebrity
+        for(int i = 0; i < n; i++){
+            if(i == pending) continue;
+            if(!knows(i, pending)) return  -1;
+        }
+        return pending;
+    }
+}
+
 
 /*
 第一遍选出候选的 celebrity，只可能有一个？
