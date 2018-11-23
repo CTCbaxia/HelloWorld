ba6e3bc8efb7d9b13c49ae9f6d792cb0502386ab
CTCbaxia <ctcbaxia@gmail.com>
2018-11-22 20:03:08 -0500

    1122 - 50

--- Medium/50-Pow(x, n).java
+++ Medium/50-Pow(x, n).java
@@ -9,6 +9,30 @@ NOTES:
 1. 对于 int，当要取负运算的时候注意边界
 2. 要学会减少复杂的子运算，用更简单的表达式代替（能储存子运算的结果，在后面调用的时候就不要重复进行该运算了）：return tmp * tmp * x; 而不是 myPow(x, n/2) * myPow(x, n/2 + 1)
 */
+/*
+Divide and Conquer
+
+1. be careful for x == 0 and n < 0 --> 1/x
+2. be careful for n < 0 : deal with it only when we use x
+Time: O(logn)  T(n) = T(n/2)
+Space: O(1)
+*/
+class Solution {
+    public double myPow(double x, int n) {
+        if(x == 0) return 0;//be careful for 1/x
+        if(n == 0) return 1;
+        
+        double num = myPow(x, n/2);
+        if(n % 2 == 0){
+            return num * num;
+        }else{
+            return (n > 0 ? x : 1/x) * (num * num);//only here we have x, so cover negative sign here
+        }
+    }
+}
+
+
+
 
 /*
 SOLUTION REFERENCE: 二分法
