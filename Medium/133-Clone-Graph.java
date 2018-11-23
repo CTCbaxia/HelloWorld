3f9358fd3f17bec76337a314f1d227c48f6e8965
CTCbaxia <ctcbaxia@gmail.com>
2018-11-22 19:03:50 -0500

    1122 - 133

--- Medium/133-Clone-Graph.java
+++ Medium/133-Clone-Graph.java
@@ -8,6 +8,42 @@ RESULT: 99% - 3ms
 NOTES: 见注释
 */
 /*
+DFS: use visited map to avoid going into dead loop
+Nodes are labeled uniquely.
+
+Time: O(n)
+Space: O(1)
+*//**
+ * Definition for undirected graph.
+ * class UndirectedGraphNode {
+ *     int label;
+ *     List<UndirectedGraphNode> neighbors;
+ *     UndirectedGraphNode(int x) { label = x; neighbors = new ArrayList<UndirectedGraphNode>(); }
+ * };
+ */
+public class Solution {
+    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+        Map<Integer, UndirectedGraphNode> visited = new HashMap<>();
+        return clone(node, visited);
+    }
+    private UndirectedGraphNode clone(UndirectedGraphNode node, Map<Integer, UndirectedGraphNode> visited){
+        if(node == null) return null;
+        else if(visited.containsKey(node.label)){
+            return visited.get(node.label);//返回 UndirectedGraphNode 之后这个值是可以持续 updated 的
+        } 
+        
+        UndirectedGraphNode copy = new UndirectedGraphNode(node.label);
+        visited.put(copy.label, copy);//必须加在 for loop 之前，因为如果 #2, 2,3,4 就会导致一开始永远在 2 处死循环 
+        for(UndirectedGraphNode n : node.neighbors){
+            copy.neighbors.add(clone(n, visited));
+        }
+        return copy;        
+    }
+}
+
+
+
+/*
 SOLUTION 0: DFS + 避免死循环
 TIME: 0830 - 30min 
 RESULT: 99% - 3ms
