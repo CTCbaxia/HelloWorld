1445e511211d6fab309432dc2556e5b2b468a5a6
CTCbaxia <ctcbaxia@gmail.com>
2018-11-23 20:36:39 -0500

    1123 - 528

--- Medium/528-Random-Pick-with-Weight.java
+++ Medium/528-Random-Pick-with-Weight.java
@@ -8,67 +8,108 @@ NOTES:
 ***** 如果要你从 O(n) 再降，如果题目涉及查找 ---- 二分法 logn
 */
 /*
-根据 ran 落在的区间找对应的位置
+直接看 range 落在哪里了，weight 越大，range也越大，随机数落在这个范围的可能性也越大
 
 Time: 
-	O(n) for initialization
-	O(logn) for pick
+    O(n) for initialization
+    O(logn) for pick (O(n)也可以，直接loop一遍) 因为 presum 是单调递增的，我们只是需要在里面寻找 target 的范围
 Space: O(n)
 */
 class Solution {
-    Random r;
+    Random rand;
     int[] sum;
     public Solution(int[] w) {
-        r = new Random();
+        rand = new Random();
         sum = new int[w.length];
-        sum[0] = w[0];
-        for(int i = 1; i < sum.length; i++){
-            sum[i] = sum[i - 1] + w[i];
+        int tmp = 0;
+        for(int i = 0; i < w.length; i++){
+            tmp += w[i];
+            sum[i] = tmp;
         }
     }
     
     public int pickIndex() {
-        int ran = r.nextInt(sum[sum.length - 1]) + 1;
-        int lo = 0; 
-        int hi = sum.length - 1;
+        int target = rand.nextInt(sum[sum.length - 1]) + 1;//随机数是 0~sum-1,但是sum里面的数是 1~sum
+        //want to find larger or equal
+        int lo = 0, hi = sum.length - 1;
         while(lo < hi){
             int mid = lo + (hi - lo)/2;
-            if(ran == sum[mid]) return mid;
-            else if(ran < sum[mid]) hi = mid;
-            else lo = mid + 1;
+            if(sum[mid] == target) return mid;
+            else if(sum[mid] < target) lo = mid + 1;
+            else hi = mid;
         }
         return hi;
     }
 }
 
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(w);
+ * int param_1 = obj.pickIndex();
+ */
 
 
 /*
-根据 ran 落在的区间找对应的位置
+直接看 range 落在哪里了，weight 越大，range也越大，随机数落在这个范围的可能性也越大
 
 Time: 
-	O(n) for initialization
-	O(n) for pick
+    O(n) for initialization
+    O(n) for pick
 Space: O(n)
 */
 class Solution {
-    Random r;
+    Random rand;
     int[] sum;
     public Solution(int[] w) {
-        r = new Random();
+        rand = new Random();
         sum = new int[w.length];
-        sum[0] = w[0];
-        for(int i = 1; i < sum.length; i++){
-            sum[i] = sum[i - 1] + w[i];
+        int tmp = 0;
+        for(int i = 0; i < w.length; i++){
+            tmp += w[i];
+            sum[i] = tmp;
         }
     }
     
     public int pickIndex() {
-        int ran = r.nextInt(sum[sum.length - 1]);
+        int r = rand.nextInt(sum[sum.length - 1]);
         for(int i = 0; i < sum.length; i++){
-            if(ran < sum[i]) return i;
+            if(r < sum[i]) return i;
         }
         return 0;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(w);
+ * int param_1 = obj.pickIndex();
+ */
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(w);
+ * int param_1 = obj.pickIndex();
+ */
+
+//借鉴其他题的方法，太慢，这题可以直接求一次，然后看落在哪里
+
+class Solution {
+    Random rand;
+    int[] weights;
+    public Solution(int[] w) {
+        weights = w;
+        rand = new Random();
+    }
+    
+    public int pickIndex() {
+        int total = 0;
+        int pending = 0;
+        for(int i = 0; i < weights.length; i++){
+            total +=weights[i];
+            int r = rand.nextInt(total);
+            if(r >= 0 && r < weights[i]) pending = i;
+        }
+        return pending;
     }
 }
 
