65029353aa7df818c7da48076e02a5af8864f025
CTCbaxia <ctcbaxia@gmail.com>
2018-11-24 17:51:13 -0500

    1124 - 348

--- Medium/348-Design-Tic-Tac-Toe.java
+++ Medium/348-Design-Tic-Tac-Toe.java
@@ -8,6 +8,66 @@ RESULT: 89% - 65ms
 
 */
 /*
+每次有人下棋，就看下的那一个子，有没有能赢
+
+Time: O(n)
+Space: O(1)
+*/
+class TicTacToe {
+    private int[][] grid;
+    private int size;
+    /** Initialize your data structure here. */
+    public TicTacToe(int n) {
+        grid = new int[n][n];
+        size = n;
+    }
+    
+    /** Player {player} makes a move at ({row}, {col}).
+        @param row The row of the board.
+        @param col The column of the board.
+        @param player The player, can be either 1 or 2.
+        @return The current winning condition, can be either:
+                0: No one wins.
+                1: Player 1 wins.
+                2: Player 2 wins. */
+    public int move(int row, int col, int player) {
+        grid[row][col] = player;
+        int theRow = 0, theCol = 0, theDiag = 0, theAntiDiag = 0;
+        for(int i = 0; i < size; i++){//只用看有更新这一行的
+            if(grid[row][i] == player) theRow++;
+        }
+        for(int i = 0; i < size; i++){//只用看有更新这一列的
+            if(grid[i][col] == player) theCol++;
+        }
+        for(int i = 0; i < size; i++){
+            if(grid[i][i] == player) theDiag++;
+        }
+        for(int i = 0; i < size; i++){
+            if(grid[i][size - i - 1] == player) theAntiDiag++;
+        }
+        
+        if(theRow == size || theCol == size || theDiag == size || theAntiDiag == size) return player;
+        else return 0;
+    }
+}
+
+/**
+ * Your TicTacToe object will be instantiated and called as such:
+ * TicTacToe obj = new TicTacToe(n);
+ * int param_1 = obj.move(row,col,player);
+ */
+
+
+
+
+
+
+
+
+
+
+ 
+/*
 SOLUTION REFERENCE: 
 O(1)
 也是自己想的但是有稍微参考答案。
