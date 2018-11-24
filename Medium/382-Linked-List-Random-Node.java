2f3095829c7c47c63a73c7eebe6e31aabd80807c
CTCbaxia <ctcbaxia@gmail.com>
2018-11-23 23:22:26 -0500

    1123 - 382

--- Medium/382-Linked-List-Random-Node.java
+++ Medium/382-Linked-List-Random-Node.java
@@ -13,33 +13,41 @@ update the result while looping
 
 Time: O(n) - one pass
 Space: O(1)
- */
+*/
+
 class Solution {
-    ListNode head;
     Random rand;
+    ListNode head;
     /** @param head The linked list's head.
         Note that the head is guaranteed to be not null, so it contains at least one node. */
     public Solution(ListNode head) {
-        this.head = head;
         rand = new Random();
+        this.head = head;
     }
     
     /** Returns a random node's value. */
     public int getRandom() {
         ListNode node = head;
-        
-        int index = 1;
-        int result = 0;
+        int count = 0;
+        int res = 0;
         while(node != null){
-            if(rand.nextInt(index) == 0){
-                result = node.val;
-            }
+            if(rand.nextInt(++count) == 0) res = node.val;
             node = node.next;
-            index++;
         }
-        return result;
+        return res;
     }
 }
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(head);
+ * int param_1 = obj.getRandom();
+ */
+
+
+
+
+
 
 /*
 Find the len of the ListNode, random(len)
