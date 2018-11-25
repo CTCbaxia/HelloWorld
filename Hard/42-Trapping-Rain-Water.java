e2dad8912f8317ca9f9ba529e28a9021ebce8ee3
CTCbaxia <ctcbaxia@gmail.com>
2018-11-24 20:40:13 -0500

    1124 - 42

--- Hard/42-Trapping-Rain-Water.java
+++ Hard/42-Trapping-Rain-Water.java
@@ -1,6 +1,56 @@
 /*
 HARD
 42. Trapping Rain Water
+*/
+/*
+Brute Force:
+for each point, search leftmax and rightmax, the water can be Math.min(leftmax, rightmax)
+
+Time: O(n^2)
+Space: O(1)
+
+Dynamic Programming:
+just save leftmax and rightmax for each point
+for every point: res += Math.min(leftMax[i], rightMax[i]) - height[i];
+
+Time: O(n)
+Space: O(n)
+*/
+class Solution {
+    public int trap(int[] height) {
+        int res = 0;
+        int[] leftMax = new int[height.length];//leftMax[i] before i, what is the heighest 
+        int[] rightMax = new int[height.length];
+        int max = 0;
+        for(int i = 0; i < height.length; i++){
+            max = Math.max(max, height[i]);
+            leftMax[i] = max;
+        }
+        max = 0;
+        for(int i = height.length - 1; i >= 0; i--){
+            max = Math.max(max, height[i]);
+            rightMax[i] = max;
+        }    
+        for(int i = 0; i < height.length; i++){
+            res += Math.min(leftMax[i], rightMax[i]) - height[i];
+        }        
+        return res;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+/*
+HARD
+42. Trapping Rain Water
 https://leetcode.com/problems/trapping-rain-water/description/
 
 TIME: 0711 - 1h
