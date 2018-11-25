f78a9a3c5de8681a8a84913960211319fdb44180
CTCbaxia <ctcbaxia@gmail.com>
2018-11-25 13:27:58 -0500

    1125 - 556

--- Medium/556-Next-Greater-Element-III.java
+++ Medium/556-Next-Greater-Element-III.java
@@ -8,6 +8,60 @@ RESULT: 99% - 2ms
 NOTES: 
 1. 注意大小 Integer.MAX_VALUE
 */
+/*
+类似于 next permutation：
+找到drop，swap，对于剩下的 reverse
+这题需要：
+1. 先变成array
+2. 最后用 long 先存
+
+Time: O(1)
+Space: O(1)
+*/
+class Solution {
+    public int nextGreaterElement(int n) {
+        char[] ch = String.valueOf(n).toCharArray();
+        
+        //find the drop
+        int i, j;
+        for(i = ch.length - 2; i >= 0; i--){
+            if(ch[i] < ch[i + 1]) break;
+        }
+        if(i < 0) return -1;//no such drop find
+        
+        //swap, there are only no more than 10 digits, so just loop
+        for(j = ch.length - 1; j > i; j--){
+            if(ch[j] > ch[i]){
+                swap(ch, i, j);
+                break;
+            }
+        }
+        //reverse the later chars
+        int lo = i + 1, hi = ch.length - 1;
+        while(lo < hi){
+            swap(ch, lo++, hi--);
+        }
+        
+        //parse to long
+        long val = Long.parseLong(new String(ch));
+        return val > Integer.MAX_VALUE ? -1 : (int) val;
+    }
+    private void swap(char[] ch, int i, int j){
+        char tmp = ch[j];
+        ch[j] = ch[i];
+        ch[i] = tmp;
+        return;
+    }
+}
+
+
+
+
+
+
+
+
+
 class Solution {
     public int nextGreaterElement(int n) {
         if(n < 10) return -1;
