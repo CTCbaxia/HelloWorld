d07d831c8bf5c0caad5b83a77eb909927faaf3b1
CTCbaxia <ctcbaxia@gmail.com>
2018-11-25 21:42:09 -0500

    1125 - 116

--- Medium/116-Populating-Next-Right-Pointers-in-Each-Node.java
+++ Medium/116-Populating-Next-Right-Pointers-in-Each-Node.java
@@ -8,6 +8,123 @@ RESULT: 100% - 0ms
 NOTE:
 1. 注意每次迭代之间的联系，上一个 level 已经搭好了从左到右的桥，下一个 level 在非左右字数的搭桥时，可以依赖上一层的桥
 */
+/**
+ * Definition for binary tree with next pointer.
+ * public class TreeLinkNode {
+ *     int val;
+ *     TreeLinkNode left, right, next;
+ *     TreeLinkNode(int x) { val = x; }
+ * }
+ */
+/*
+BFS:
+每层遍历，跟着 node.next 走每一层
+在每一层遍历的时候，构建下一层的 node.next
+
+Time: O(n)
+Space: O(1)
+*/
+public class Solution {
+    public void connect(TreeLinkNode root) {
+        if(root == null) return;
+        TreeLinkNode levelHead = root;
+        TreeLinkNode node = levelHead;//for every level, store the head
+        while(levelHead.left != null){//levelHead has next relation already. See if we have next level to build
+            //connect children level from left to right following node.next
+            while(node != null){
+                node.left.next = node.right;
+                if(node.next != null) node.right.next = node.next.left;
+                node = node.next;
+            }//end of this level
+            node = levelHead.left;//move to the next level
+            levelHead = node;
+        }
+        return;
+    }
+}
+
+
+
+
+
+
+
+
+/**
+ * Definition for binary tree with next pointer.
+ * public class TreeLinkNode {
+ *     int val;
+ *     TreeLinkNode left, right, next;
+ *     TreeLinkNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public void connect(TreeLinkNode root) {
+        TreeLinkNode node = root;
+        while(node != null){
+            //connect left and right
+            if(node.left != null){
+                node.left.next = node.right;
+            }
+            if(node.next != null){
+                node.right.next = node.next.left;
+                node = node.next;
+            }else node = node.left;
+        }
+        return;
+    }
+}
+
+
+
+
+
+
+
+
+/*
+BFS
+put every level into queue from right to the left
+
+Time: O(n)
+Space: O(n)
+*/
+/**
+ * Definition for binary tree with next pointer.
+ * public class TreeLinkNode {
+ *     int val;
+ *     TreeLinkNode left, right, next;
+ *     TreeLinkNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public void connect(TreeLinkNode root) {
+        Queue<TreeLinkNode> q = new LinkedList<>();
+        if(root == null) return;
+        q.offer(root);
+        while(!q.isEmpty()){
+            Queue<TreeLinkNode> newQ = new LinkedList<>();
+            TreeLinkNode end = null;
+            while(!q.isEmpty()){
+                TreeLinkNode node = q.poll();
+                node.next = end;
+                end = node;
+                
+                //put children into newQ
+                if(node.right != null) newQ.offer(node.right);
+                if(node.left != null) newQ.offer(node.left);
+            }
+            q = newQ;
+        }
+        return;
+    }
+}
+
+
+
+
+
+
 
 
 /*
