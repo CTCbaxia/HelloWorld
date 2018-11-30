17093b9e7cc77ad74f80e8bb54f4c6bb04037553
CTCbaxia <ctcbaxia@gmail.com>
2018-11-29 22:30:38 -0500

    1129 - 281

--- Medium/281-Zigzag-Iterator.java
+++ Medium/281-Zigzag-Iterator.java
@@ -8,6 +8,43 @@ NOTES:
 
 */
 /*
+Iterator + Queue for kind of BFS:
+if the iterator hasNext, we put it into queue
+everytime we poll first, and offer back if it continuously have next (then it will be the end of the queue)
+
+Time: O(n)
+Space: O(1)
+*/
+public class ZigzagIterator {
+    Queue<Iterator> q;
+    public ZigzagIterator(List<Integer> v1, List<Integer> v2) {
+        q = new LinkedList<Iterator>();
+        Iterator<Integer> it1 = v1.iterator();
+        Iterator<Integer> it2 = v2.iterator();
+        if(it1.hasNext()) q.offer(it1);
+        if(it2.hasNext()) q.offer(it2);
+    }
+
+    public int next() {
+        Iterator<Integer> it = q.poll();
+        int tmp = it.next();
+        if(it.hasNext()) q.offer(it);
+        return tmp;
+    }
+
+    public boolean hasNext() {
+        return !q.isEmpty();
+    }
+}
+
+/**
+ * Your ZigzagIterator object will be instantiated and called as such:
+ * ZigzagIterator i = new ZigzagIterator(v1, v2);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
+
+
+/*
 Iterator + kind of BFS:
 if the iterator hasNext, we put it into queue
 everytime we poll first, and offer back if it continuously have next (then it will join the queue)
