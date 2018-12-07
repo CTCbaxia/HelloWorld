6c40c78fd621c28c3fa68465e4ec57cc712581be
CTCbaxia <ctcbaxia@gmail.com>
2018-12-06 21:25:58 -0500

    1206 - 79

--- Medium/79-Word-Search.java
+++ Medium/79-Word-Search.java
@@ -5,6 +5,102 @@ https://leetcode.com/problems/word-search/description/
 TIME: 0710
 RESULT: 99.5%, 8ms
 NOTE:
+
+*/
+/*
+DFS + backtracking (Set for visited)
+
+Time: O(mn * 4^len)
+Space: O(mn) + O(k) //recursive //can also modify the board to save space c -> '\0'
+*/
+class Solution {
+    public boolean exist(char[][] board, String word){
+        if(board.length == 0 || board[0].length == 0) return word.length() == 0;
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[0].length; j++){
+                if(board[i][j] == word.charAt(0)){
+                    if(search(board, word, 1, i, j, new HashSet<Integer>())) return true;
+                }
+            }
+        }
+        return false;
+    }
+    private boolean search(char[][] board, String word, int index, int i, int j, Set<Integer> visited){
+        if(index == word.length()) return true;
+        
+        visited.add(i * board[0].length + j);
+        
+        int[][] direction = new int[][]{{1, 0}, {-1, 0},{0, 1}, {0, -1}};
+        for(int[] dir : direction){
+            int row = i + dir[0];
+            int col = j + dir[1];
+            if(row >= 0 && row < board.length && col >= 0 && col < board[0].length){
+                if(visited.contains(row * board[0].length + col)) continue;
+                if(board[row][col] == word.charAt(index)){
+                    if(search(board, word, index + 1, row, col, visited)) return true;
+                }
+            }
+        }
+        visited.remove(i * board[0].length + j);
+        return false;
+    }
+}
+
+
+
+/*
+DFS + backtracking (Set for visited)
+
+Time: O(mn * 4^len)
+Space: O(k) //recursive
+*/
+class Solution {
+    public boolean exist(char[][] board, String word){
+        if(board.length == 0 || board[0].length == 0) return word.length() == 0;
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[0].length; j++){
+                if(board[i][j] == word.charAt(0)){
+                    if(search(board, word, 1, i, j)) return true;
+                }
+            }
+        }
+        return false;
+    }
+    private boolean search(char[][] board, String word, int index, int i, int j){
+        if(index == word.length()) return true;
+        
+        char c = board[i][j];
+        board[i][j] = '\0';
+        
+        int[][] direction = new int[][]{{1, 0}, {-1, 0},{0, 1}, {0, -1}};
+        for(int[] dir : direction){
+            int row = i + dir[0];
+            int col = j + dir[1];
+            if(row >= 0 && row < board.length && col >= 0 && col < board[0].length){
+                if(board[row][col] == word.charAt(index)){
+                    if(search(board, word, index + 1, row, col)) return true;
+                }
+            }
+        }
+        board[i][j] = c;
+        return false;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 1. 让一个 char 数组为 空：board[row][column] = '\0';
 2. 注意传参： 数组，List 都是传的地址（改变的是原始值）；而 boolean， string， int 是复制 value
 3. 注意方法的执行时序，只需要保证在执行该方法的时候 '/0' 的限制有效就行，在执行方法之后可以还原 board
@@ -13,7 +109,6 @@ NOTE:
 QUESTION:
 不是很懂 private void 的写法为什么就那么慢？？
 */
-
 class Solution {
     public boolean exist(char[][] board, String word) {
         
