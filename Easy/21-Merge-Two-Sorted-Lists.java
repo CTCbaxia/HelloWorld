bd738778cf4e10f3596af32e1c432d62c399ba34
CTCbaxia <ctcbaxia@gmail.com>
2018-12-07 16:41:44 -0500

    1207 - 21

--- Easy/21-Merge-Two-Sorted-Lists.java
+++ Easy/21-Merge-Two-Sorted-Lists.java
@@ -1,6 +1,41 @@
 /*
 21. Merge Two Sorted Lists
-*/          
+*/       
+/*
+Two Pointers
+
+Time: O(n)
+Space: O(1)
+*/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2){
+        ListNode res = new ListNode(0);
+        ListNode node = res;
+        while(l1 != null && l2 != null){
+            if(l1.val <= l2.val){
+                node.next = l1;
+                l1 = l1.next;
+                node = node.next;
+            }else{
+                node.next = l2;
+                l2 = l2.next;
+                node = node.next;
+            }
+        }
+        if(l1 != null) node.next = l1;
+        if(l2 != null) node.next = l2;
+        return res.next;
+    }
+
+}   
 /**
  * Definition for singly-linked list.
  * public class ListNode {
