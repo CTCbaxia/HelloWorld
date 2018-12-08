c9ea1f8809c720353b7161fc8b48dfb4f00eb237
CTCbaxia <ctcbaxia@gmail.com>
2018-12-07 20:36:54 -0500

    1207 - 622

--- Medium/622-Design-Circular-Queue.java
+++ Medium/622-Design-Circular-Queue.java
@@ -9,6 +9,75 @@ NOTES:
 */
 /*
 Array
+限制大小的array，用 head, tail 来作为位移，指向头尾部分
+deQueue: head = (head + 1) % q.length;
+enQueue: tail = (tail + 1) % q.length;
+用 num 来计数，看目前到底有几个 nums
+
+Time: O(n)
+Space: O(n)
+*/
+class MyCircularQueue {
+    int[] q;
+    int head;
+    int tail;
+    int num;
+    /** Initialize your data structure here. Set the size of the queue to be k. */
+    public MyCircularQueue(int k) {
+        q = new int[k];
+        head = 0;
+        tail = -1;
+        num = 0;
+    }
+    
+    /** Insert an element into the circular queue. Return true if the operation is successful. */
+    public boolean enQueue(int value) {
+        if(isFull()) return false;
+        
+        tail = (tail + 1)%q.length;
+        q[tail] = value;
+        num++;
+        return true;
+    }
+    
+    /** Delete an element from the circular queue. Return true if the operation is successful. */
+    public boolean deQueue() {
+        if(isEmpty()) return false;
+        
+        head = (head + 1)%q.length;
+        num--;
+        return true;
+    }
+    
+    /** Get the front item from the queue. */
+    public int Front() {
+        if(isEmpty()) return -1;
+        return q[head];
+    }
+    
+    /** Get the last item from the queue. */
+    public int Rear() {
+        if(isEmpty()) return -1;
+        return q[tail];
+        
+    }
+    
+    /** Checks whether the circular queue is empty or not. */
+    public boolean isEmpty() {
+        return num == 0;
+    }
+    
+    /** Checks whether the circular queue is full or not. */
+    public boolean isFull() {
+        return num == q.length;
+    }
+}
+
+
+
+
+/*
+Array
 限制大小的array，用 front, rear 来处理最后的头尾部分
 deQueue: front = (front + 1) % q.length;
 enQueue: rear = (rear + 1) % q.length;
