e4ebd490132c00141ff7fcd89f201bd4cbb9c2af
CTCbaxia <ctcbaxia@gmail.com>
2018-12-09 18:16:37 -0500

    1209 - 100

--- Easy/100-Same-Tree.java
+++ Easy/100-Same-Tree.java
@@ -17,7 +17,7 @@ Space: O(1)
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if(p == null) return q == null;
-        if(q == null) return p == null;
+        if(q == null) return p == null;//双向 check 很重要
         if(p.val != q.val) return false;
         return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
