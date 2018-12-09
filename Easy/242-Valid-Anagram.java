c344c6b93a5d9d7b10fc7a2d763b40daddfd1ee4
CTCbaxia <ctcbaxia@gmail.com>
2018-12-09 16:50:15 -0500

    1209 - 242

--- Easy/242-Valid-Anagram.java
+++ Easy/242-Valid-Anagram.java
@@ -1,11 +1,102 @@
 /*
 242. Valid Anagram
-https://leetcode.com/problems/valid-anagram/description/
 
 TIME: 0709
 RESULT: 14%, 47ms
 Notes: 
 */
+/*
+计算频度（int[26] count），一加一减
+只有所有字母count == 0 的时候才return true
+
+Time: O(n)
+Space: O(26)
+*/
+class Solution {
+    public boolean isAnagram(String s, String t){
+        int[] count = new int[26];
+        for(int i = 0; i < s.length(); i++) count[s.charAt(i) - 'a']++;
+        for(int i = 0; i < t.length(); i++) count[t.charAt(i) - 'a']--;
+        for(int i = 0; i < count.length; i++){
+            if(count[i] != 0) return false;
+        }
+        return true;
+    }
+}
+
+
+/*
+排序 
+Time: O(nlogn) 
+Space: O(n)
+*/
+public boolean isAnagram(String s, String t){
+    if(s.length() != t.length()) return false;
+    char[] sch = s.toCharArray();
+    char[] tch = t.toCharArray();
+    
+    Arrays.sort(sch);
+    Arrays.sort(tch);
+    for(int i = 0; i < s.length(); i++){
+        if(sch[i] != tch[i]) return false;
+    }
+    return true;
+} 
+
+
+/*
+Map 频度
+Time: O(n)
+Space: O(26)
+*/
+public boolean isAnagram(String s, String t){
+    if(s.length() != t.length()) return false;
+    Map<Character, Integer> map = new HashMap<>();
+    for(int i = 0; i < s.length(); i++){
+        char c = s.charAt(i);
+        map.put(c, map.getOrDefault(c, 0) + 1);
+    }
+    int key = map.size();
+    for(int i = 0; i < t.length(); i++){
+        char c = t.charAt(i);
+        if(!map.containsKey(c)) return false;
+        map.put(c, map.get(c) - 1);
+        if(map.get(c) == 0) {
+            key--;
+            map.remove(c);
+        }
+    }
+    return key == 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length()){
