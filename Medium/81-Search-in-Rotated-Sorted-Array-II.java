002a9944816ec92fe577cf29dc917ee806082f32
CTCbaxia <ctcbaxia@gmail.com>
2018-12-10 16:53:33 -0500

    1210 - 81

--- Medium/81-Search-in-Rotated-Sorted-Array-II.java
+++ Medium/81-Search-in-Rotated-Sorted-Array-II.java
@@ -6,10 +6,41 @@ https://leetcode.com/problems/search-in-rotated-sorted-array-ii/description/
 TIME: 0902 - 1h
 RESULT: 100% - 0ms
 NOTES:
-其实这题的重点是如何确定哪里是顺序，哪里是断层。
-如果无法确定（当没有大小区别的时候），就直接慢慢顺移，继续比较，不要匆忙二分法。
-顺序或断层，得遵循绝对的大小区别，等于的情况无法判断
+
 */
+/*
+Two Pointer + Binary Search
+先除去头尾的重复元素，至少留两个元素 -- 只有他们会干扰判断方向
+然后跟非重复一样判断 target 是否落在单调区间内
+
+Time: O(n) - O(logn)
+Space: O(1)
+*/
+class Solution {
+    public boolean search(int[] nums, int target){
+        int lo = 0, hi = nums.length - 1;
+        while(lo + 1 < hi && nums[lo] == nums[lo + 1]) lo++;
+        while(hi - 1 > lo && nums[hi] == nums[hi - 1]) hi--;
+        
+        /*here lo and hi may still be the same, but mid will not be same as hi until there is only two elements*/
+        while(lo <= hi){// we want to check for the target, so use <=
+            int mid = lo + (hi - lo) / 2;
+            if(nums[mid] == target) return true;
+
+            if(nums[mid] <= nums[hi]){//check with hi to see: right is increasing
+                if(target > nums[mid] && target <= nums[hi]) lo = mid + 1;
+                else hi = mid - 1;
+            }else{
+                if(target < nums[mid] && target >= nums[lo]) hi = mid - 1;
+                else lo = mid + 1;
+            }
+        }
+        return false;
+    }    
+}
+
+
+
 /*
 Solution 1
 when it is hard to decide, we move hi one step to the left and see
@@ -86,6 +117,9 @@ class Solution {
 
 /*
 SOLUTION REFERENCE: 更简化
+其实这题的重点是如何确定哪里是顺序，哪里是断层。
+如果无法确定（当没有大小区别的时候），就直接慢慢顺移，继续比较，不要匆忙二分法。
+顺序或断层，得遵循绝对的大小区别，等于的情况无法判断
 TIME: 0902 - 1h
 RESULT: 100% - 0ms
 */
