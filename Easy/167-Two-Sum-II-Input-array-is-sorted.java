0078bcf21b50bb725ff4c5c015562b7755d9d821
CTCbaxia <ctcbaxia@gmail.com>
2018-12-09 22:11:33 -0500

    1209 - 167

--- Easy/167-Two-Sum-II-Input-array-is-sorted.java
+++ Easy/167-Two-Sum-II-Input-array-is-sorted.java
@@ -8,6 +8,27 @@ NOTES:
 2 Sum 其实也可以用排序的方法
 
 */
+/*
+Two Pointers
+
+Time: O(n)
+Space: O(1)
+*/
+class Solution {
+    public int[] twoSum(int[] numbers, int t){
+        int lo = 0, hi = numbers.length - 1;
+        while(lo < hi){
+            int sum = numbers[lo] + numbers[hi];
+            if(sum < t) lo++;
+            else if(sum > t) hi--;
+            else return new int[]{lo + 1, hi + 1};
+        }
+        return null;
+    } 
+}
+
+
+
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
         int lo = 0, hi = numbers.length - 1;
