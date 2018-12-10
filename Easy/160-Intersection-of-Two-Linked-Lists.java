79eb930e060b82c6d55ec12f01f94e416426fbe7
CTCbaxia <ctcbaxia@gmail.com>
2018-12-10 18:01:43 -0500

    1210 - 160

--- Easy/160-Intersection-of-Two-Linked-Lists.java
+++ Easy/160-Intersection-of-Two-Linked-Lists.java
@@ -1,6 +1,6 @@
 /*
 EASY
-606-Construct-String-from-Binary-Tree.java
+160. Intersection of Two Linked Lists
 https://leetcode.com/problems/construct-string-from-binary-tree/description/
 
 TIME: 1011 - 15min
@@ -8,6 +8,68 @@ RESULT: 100% - 1ms
 NOTES: 
 找到长度，然后从同一个起点开始
 */
+/*
+计算长度 + 从同一个地方开始
+
+Time: O(n)
+Space: O(1)
+*/
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB){
+        int counta = 0;
+        int countb = 0;
+        ListNode nodea = headA;
+        ListNode nodeb = headB;
+
+        while(nodea != null){
+            counta++;
+            nodea = nodea.next;
+        }
+        while(nodeb != null){
+            countb++;
+            nodeb = nodeb.next;
+        }
+        nodea = headA;
+        nodeb = headB;
+        if(counta >= countb){
+            int diff = counta - countb;
+            while(diff-- > 0) nodea = nodea.next;
+        }else{
+            int diff = countb - counta;
+            while(diff-- > 0) nodeb = nodeb.next;
+        }
+
+        while(nodea != null && nodeb != null){
+            if(nodea == nodeb) return nodea;
+            nodea = nodea.next;
+            nodeb = nodeb.next;
+        }
+        return null;
+    }    
+   
+}
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+
+
+
+
+
+
+
+
+
+
 //其实只要让他们同时到达 inetrsection就好
 /**
  * Definition for singly-linked list.
