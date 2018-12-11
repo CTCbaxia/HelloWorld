148e006188ddbeef1e78e2dc87d8e5f3022d3434
CTCbaxia <ctcbaxia@gmail.com>
2018-12-11 13:02:28 -0500

    1211 - 36

--- Medium/36-Valid-Sudoku.java
+++ Medium/36-Valid-Sudoku.java
@@ -15,6 +15,45 @@ Space: O(n)
 */
 class Solution {
     public boolean isValidSudoku(char[][] board) {
+        List<Set<Character>> cols = new ArrayList<>();
+        List<Set<Character>> boards = new ArrayList<>();
+
+        for(int i = 0; i < board.length; i++){
+            cols.add(new HashSet<Character>());
+            boards.add(new HashSet<Character>());
+        }
+
+        for(int i = 0; i < board.length; i++){
+            Set<Character> row = new HashSet<>();
+            for(int j = 0; j < board[0].length; j++){
+                char c = board[i][j];
+                if(c == '.') continue;
+                int boardNum = i / 3 * 3 + j / 3;
+                if(row.contains(c) || cols.get(j).contains(c) || boards.get(boardNum).contains(c)){
+                    return false;
+                }
+                row.add(c);
+                cols.get(j).add(c);
+                boards.get(boardNum).add(c);
+            }
+        }
+        return true;
+    }  
+}
+
+
+
+
+
+
+/*
+One Pass: Find the right set to check
+
+Time: O(n)
+Space: O(n)
+*/
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
         List<Set<Character>> subboards = new ArrayList<>();
         List<Set<Character>> cols = new ArrayList<>();
         //initialization
