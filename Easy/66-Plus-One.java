4f331a1752ae0cd07022c7923a56a0af25eaeea2
CTCbaxia <ctcbaxia@gmail.com>
2018-12-11 12:29:53 -0500

    1211 - 66

--- Easy/66-Plus-One.java
+++ Easy/66-Plus-One.java
@@ -6,6 +6,34 @@ TIME: 0706
 RESULT: 100%
 NOTE: easy
 */
+/*
+数学相加(carry)
+
+Time: O(n)
+Space: O(n)
+*/
+class Solution {
+    public int[] plusOne(int[] digits) {
+        List<Integer> res = new ArrayList<>();
+        int carry = 1;
+        for(int i = digits.length - 1; i >= 0; i--){
+            int num = carry + digits[i];
+            carry = num / 10;
+            num = num % 10;
+            res.add(0, num);
+        }
+        if(carry > 0) res.add(0, carry);
+        int[] result = new int[res.size()];
+        for(int i = 0; i < result.length; i++){
+            result[i] = res.get(i);
+        }
+        return result;
+    }
+}  
+
+
+
+
 
 class Solution {
     public int[] plusOne(int[] digits) {
