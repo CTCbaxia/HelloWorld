b82b52a25166fe312f872cb22ace2dd0a46c8b90
CTCbaxia <ctcbaxia@gmail.com>
2018-12-12 12:20:10 -0500

    1212 - 118

--- Easy/118-Pascal's-Triangle.java
+++ Easy/118-Pascal's-Triangle.java
@@ -7,6 +7,70 @@ RESULT:
 NOTES:
 */
 /*
+Iteration: Add Level by Level
+
+Time: O(n)
+Space: O(1)
+*/
+class Solution {
+    public List<List<Integer>> generate(int numRows){
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> cur = new ArrayList<>();
+        if(numRows <= 0) return result;
+        
+        cur.add(1);
+        result.add(cur);
+        
+        while(result.size() < numRows){
+            List<Integer> pre = result.get(result.size() - 1);
+            cur = new ArrayList<>();
+            cur.add(1);
+            for(int i = 0; i < pre.size() - 1; i++){
+                cur.add(pre.get(i) + pre.get(i + 1));
+            }
+            cur.add(1);
+            result.add(cur);
+        }
+        return result;        
+    }
+
+}
+/*
+Recursion: Add Level by Level
+
+Time: O(n)
+Space: O(1)
+*/
+class Solution {
+    public List<List<Integer>> generate(int numRows){
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> res = new ArrayList<>();
+        if(numRows <= 0) return result;
+        
+        res.add(1);
+        result.add(res);
+        buildLevel(1, numRows, result);
+        return result;        
+    }
+    private void buildLevel(int level, int numRows, List<List<Integer>> result){
+        if(level == numRows) return;
+
+        List<Integer> pre = result.get(level - 1);
+        List<Integer> cur = new ArrayList<>();
+        cur.add(1);
+        for(int i = 0; i < pre.size() - 1; i++){
+            cur.add(pre.get(i) + pre.get(i + 1));
+        }
+        cur.add(1);
+        result.add(cur);
+        
+        buildLevel(level + 1, numRows, result);
+        return;
+    }
+}
+
+
+/*
 Add Level by Level
 
 Time: O(n)
