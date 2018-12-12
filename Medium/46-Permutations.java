bf8cf0078fddb05634429a517f1b3da56822bc06
CTCbaxia <ctcbaxia@gmail.com>
2018-12-12 11:52:25 -0500

    1212 - 46

--- Medium/46-Permutations.java
+++ Medium/46-Permutations.java
@@ -1,12 +1,38 @@
 /*
 MEDIUM
 46. Permutations
-
-
 TIME: 
 RESULT: 30% - 5ms
 
 */
+/*
+DFS(backtracking) + list 自带去重
+
+Time: O(n!)
+Space: O(n)
+*/
+class Solution {
+    public List<List<Integer>> permute(int[] nums){
+        List<List<Integer>> result = new ArrayList<>();
+        dfs(nums, result, new ArrayList<Integer>());
+        return result;
+    }   
+    private void dfs(int[] nums, List<List<Integer>> result, List<Integer> res){
+        if(res.size() == nums.length) result.add(new ArrayList<Integer>(res));
+
+        for(int i = 0; i < nums.length; i++){
+            if(res.contains(nums[i])) continue;
+            res.add(nums[i]);
+            dfs(nums, result, res);
+            res.remove(res.size() - 1);//backtracking
+        }
+        return;
+    }
+}
+
+
+
+
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
