149aae44270538dcc1b2e73e5b32f1562e243131
CTCbaxia <ctcbaxia@gmail.com>
2018-12-12 12:37:31 -0500

    1212 - 38

--- Easy/38-Count-and-Say.java
+++ Easy/38-Count-and-Say.java
@@ -4,11 +4,39 @@ https://leetcode.com/problems/find-pivot-index/description/
 
 TIME: 0713 - 30min
 RESULT: 7% - 29ms
-Notes: 
-
-METHOD:
 
 */
+/*
+Iteration: while 同类元素 get count
+
+Time: O(n)
+Space: O(n)
+*/
+class Solution {
+    public String countAndSay(int n) {
+        if(n == 0) return "";
+        String cur = "1";
+        while(--n > 0){
+            String pre = cur;
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i < pre.length(); i++){
+                int count = 1;
+                while(i + 1 < pre.length() && pre.charAt(i) == pre.charAt(i + 1)){
+                    count++;
+                    i++;
+                }
+                sb.append(count).append(pre.charAt(i));
+            }
+            cur = sb.toString();
+        }
+        return cur;
+    }   
+}
+
+
+
+
+
 
 class Solution {
     public String countAndSay(int n) {
