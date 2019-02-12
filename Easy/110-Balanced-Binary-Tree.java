0c6a88fedb312b5d66c5445473dba6b0fa7442dd
CTCbaxia <ctcbaxia@gmail.com>
2019-02-11 23:30:56 -0500

    0211

--- Easy/110-Balanced-Binary-Tree.java
+++ Easy/110-Balanced-Binary-Tree.java
@@ -33,6 +33,27 @@ class Solution {
 }
 
 
+//use outer flag
+class Solution {
+    public boolean isB = true;
+    public boolean isBalanced(TreeNode root) {
+        helper(root);
+        return isB;
+    }
+    private int helper(TreeNode node){
+        if(node == null) return 0;
+        int left = helper(node.left);
+        int right = helper(node.right);
+        
+        
+        if(Math.abs(left - right) > 1) isB = false;
+        
+        return Math.max(left, right) + 1;
+    }
+}
+
+
+
 
 
 //use class
