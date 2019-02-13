3c266fd7f19f46cc9109b44b0a33d30d44b6f25a
CTCbaxia <ctcbaxia@gmail.com>
2019-02-12 21:11:58 -0500

    0212

---
+++ BasicAlgo/mergeSort.java
@@ -0,0 +1,44 @@
+/*
+Merge Sort
+
+Time: 
+Worst case -- O(nlogn) --- T(n) = 2T(n/2) + O(n)
+Space: 
+O(n)  as we’re creating temporary arrays in every recursive call.
+*/
+public static void mergeSort(int[] a, int n) {
+    if (n < 2) return;
+
+    int mid = n / 2;
+    int[] l = new int[mid];
+    int[] r = new int[n - mid];
+ 
+    for (int i = 0; i < mid; i++) {
+        l[i] = a[i];
+    }
+    for (int i = mid; i < n; i++) {
+        r[i - mid] = a[i];
+    }
+    //分成左半边和右半边
+    mergeSort(l, mid);
+    mergeSort(r, n - mid);
+    
+    merge(a, l, r, mid, n - mid);
+}
+public static void merge(int[] a, int[] l, int[] r, int left, int right) {
+  
+    int i = 0, j = 0, k = 0;
+    while (i < left && j < right) {
+        if (l[i] <= r[j]) {
+            a[k++] = l[i++];
+        }else {
+            a[k++] = r[j++];
+        }
+    }
+    while (i < left) {
+        a[k++] = l[i++];
+    }
+    while (j < right) {
+        a[k++] = r[j++];
+    }
+}
