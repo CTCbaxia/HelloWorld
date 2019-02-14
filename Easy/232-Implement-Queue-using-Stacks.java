c19c520dffb0c38f418305118632d60ef96b5daa
CTCbaxia <ctcbaxia@gmail.com>
2019-02-14 00:01:37 -0500

    0213 - 232

--- Easy/232-Implement-Queue-using-Stacks.java
+++ Easy/232-Implement-Queue-using-Stacks.java
@@ -13,6 +13,58 @@ NOTE:
 主要就是改变 push 之后的结果
 */
 class MyQueue {
+    Stack<Integer> myQ;
+    /** Initialize your data structure here. */
+    public MyQueue() {
+        myQ = new Stack<>();
+    }
+    
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        Stack<Integer> helper = new Stack<>();
+        while(!myQ.isEmpty()){
+            helper.push(myQ.pop());
+        }
+        myQ.push(x);
+        while(!helper.isEmpty()){
+            myQ.push(helper.pop());
+        }
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        return myQ.pop();
+    }
+    
+    /** Get the front element. */
+    public int peek() {
+        return myQ.peek();
+    }
+    
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return myQ.isEmpty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
+
+
+
+
+
+
+
+
+ 
+class MyQueue {
     Stack<Integer> myque = new Stack<Integer>();
     Stack<Integer> reverse = new Stack<Integer>();
 
@@ -47,54 +99,3 @@ class MyQueue {
         return myque.empty();
     }
 }
-
-
-/*
-只给了 stack 的基本用法，没给迭代，所以 foreach 行不通
-*/
-class MyQueue {
-    Stack<Integer> myque = new Stack<Integer>();
-    Stack<Integer> reverse = new Stack<Integer>();
-
-    /** Initialize your data structure here. */
-    public MyQueue() {
-        
-    }
-    
-    /** Push element x to the back of queue. */
-    public void push(int x) {
-        for(int i : reverse){
-            myque.pop();
-        }
-        reverse.push(x);
-        for(int j : reverse){
-            myque.push(j);
-        }
-
-    }
-    
-    /** Removes the element from in front of queue and returns that element. */
-    public int pop() {
-        return myque.pop();
-    }
-    
-    /** Get the front element. */
-    public int peek() {
-        return myque.peek();
-    }
-    
-    /** Returns whether the queue is empty. */
-    public boolean empty() {
-        return myque.empty();
-    }
-}
-
-
-/**
- * Your MyQueue object will be instantiated and called as such:
- * MyQueue obj = new MyQueue();
- * obj.push(x);
- * int param_2 = obj.pop();
- * int param_3 = obj.peek();
- * boolean param_4 = obj.empty();
- */
