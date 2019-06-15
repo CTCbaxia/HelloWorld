772ba53d8beafa26523777fe6e420ce5ec19a9bf
CTCbaxia <ctcbaxia@gmail.com>
2019-06-15 01:01:20 -0700

    0614 - 1027. Longest Arithmetic Sequence

---
+++ Medium/1027-Longest-Arithmetic-Sequence.java
@@ -0,0 +1,96 @@
+/*
+MEDIUM
+1027. Longest Arithmetic Sequence
+*/
+/*
+DP + List of Map
+Map<distance, len>
+
+
+Time: O(n^2) = use map to make finding at O(1)
+Space: O(n^2)
+
+*/
+class Solution {
+    public int longestArithSeqLength(int[] A) {
+        Map<Integer, Integer>[] dp = new HashMap[A.length];
+        int res = 0;
+        for(int i = 0; i < A.length; i++){
+            dp[i] = new HashMap<Integer, Integer>();
+            for(int j = 0; j < i; j++){
+                int dis = A[i] - A[j];
+                int len = dp[j].getOrDefault(dis, 1) + 1;
+                dp[i].put(dis, len);
+                res = Math.max(res, len);
+            }
+        }
+        return res;
+    }
+
+}
+
+
+/*
+DP + List of Map
+Map<distance, len>
+
+
+Time: O(n^2) = use map to make finding at O(1)
+Space: O(n^2)
+
+*/
+class Solution {
+    public int longestArithSeqLength(int[] A) {
+        List<Map<Integer, Integer>> list = new ArrayList<>();
+        int res = 0;
+        for(int i = 0; i < A.length; i++){
+            list.add(new HashMap<Integer, Integer>());
+            for(int j = 0; j < i; j++){
+                int dis = A[i] - A[j];
+                int len = list.get(j).containsKey(dis) ? list.get(j).get(dis) : 1;
+                list.get(i).put(dis, len + 1);
+                res = Math.max(res, len + 1);
+            }
+        }
+        return res;
+    }
+
+}
+
+
+
+/*
+DP?
+Time: O(n^3) = n^2 * findAS[O(n)]
+Space: O(n^2)
+
+*/
+class Solution {
+    public int longestArithSeqLength(int[] A) {
+        int n = A.length;
+        int[][] dis = new int[n][n];
+        for(int i = 0; i < n; i++){
+            for(int j = i + 1; j < n; j++){
+                dis[i][j] = A[j] - A[i];
+            }
+        }
+        int res = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = i + 1; j < n; j++){
+                res = Math.max(res, findAS(dis, j, dis[i][j], 2));
+            }
+        }
+        return res;
+    }
+    private int findAS(int[][] dis, int i, int d, int len){
+        //d: the distance between each num
+        //len: len of the subsequence
+        for(int j = i + 1; j < dis[0].length; j++){
+            if(dis[i][j] == d){
+                return findAS(dis, j, d, len + 1);
+            }
+        }
+        return len;
+        
+    }
+}
