01bb94b5f755752a7f4628438c6be68108cc51b5
CTCbaxia <ctcbaxia@gmail.com>
2019-06-21 01:25:59 -0700

    0620 - 再看

--- Medium/714-Best-Time-to-Buy-and-Sell-Stock-with-Transaction-Fee.java
+++ Medium/714-Best-Time-to-Buy-and-Sell-Stock-with-Transaction-Fee.java
@@ -1,42 +1,42 @@
 /*
 MEDIUM
-939. Minimum Area Rectangle
+714. Best Time to Buy and Sell Stock with Transaction Fee
 
 */
 /*
-Map
-1. 遍历所有点，build map
-2. 遍历所有点，看看这两个点构成的 rect 是否更小，再看可不可以找到能够合成 rect 的其他点 -- 这样复杂度会更低
+DP
+dp[i][j]: largest revenue between i and j
 
-Time: O(n^2)
-Space: O(n)
+Initialization:
+
+dp[i][i] = 0;
+
+Transform:  dp[i][j] should always be >= 0
+i  k   j
+1) only do transaction with the [i],[j] value
+    dp[i][j] = Math.max(dp[i][j], prices[j] - prices[i] - fee);
+2) if do transaction between i and j
+    dp[i][j] = Math.max(dp[i][j], dp[i][i + k] + dp[i + k + 1][j]);
+    
+Time: O(n^3)
+Space: O(n^2)
 */
 class Solution {
-    public int minAreaRect(int[][] points) {
-        Map<Integer, List<Integer>> map = new HashMap<>();
-        for(int[] p : points){
-            if(!map.containsKey(p[0])) map.put(p[0], new ArrayList<>());
-            map.get(p[0]).add(p[1]);
-        }
-        int res = Integer.MAX_VALUE;
-        //这里遍历 points 而非 map
-        for(int i = 0; i < points.length; i++){
-            for(int j = i + 1; j < points.length; j++){
-                int[] p1 = points[i];
-                int[] p2 = points[j];
-                if(p1[0] == p2[0] || p1[1] == p2[1]) continue;//if there share same x or y
-                //get two 对角线的点
-                int area = Math.abs(p1[0] - p2[0]) * Math.abs(p1[1] - p2[1]);
-                if(area < res){
-                    //❤️这里 tricky 的点是：先算值是否更小，再看这是不是 rect
-                    //因为 list.contains() 的操作是 O(n)
-                    if(map.get(p1[0]).contains(p2[1]) && map.get(p2[0]).contains(p1[1])){
-                        res = area;
-                    }  
+    public int maxProfit(int[] prices, int fee) {
+        int[][] dp = new int[prices.length][prices.length];
+        for(int i = 0; i < dp.length; i++){
+            dp[i][i] = 0;
+        } 
+        for(int i = dp.length - 1; i >= 0; i--){
+            for(int j = i + 1; j < dp[i].length; j++){
+                //if only do transaction with the [i],[j] value
+                dp[i][j] = Math.max(dp[i][j], prices[j] - prices[i] - fee);
+                for(int k = 0; k <= j - i - 1;  k++){
+                    //if do transaction between [i],[j] value
+                    dp[i][j] = Math.max(dp[i][j], dp[i][i + k] + dp[i + k + 1][j]);
                 }
-                
             }
         }
-        return res == Integer.MAX_VALUE ? 0 : res;
+        return dp[0][prices.length - 1];
     }
 }
