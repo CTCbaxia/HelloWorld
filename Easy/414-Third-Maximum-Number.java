9927152934bff7a18cff223c5ed4cd5eefdd1bc5
CTCbaxia <ctcbaxia@gmail.com>
2019-07-02 00:00:25 -0700

    Update 414-Third-Maximum-Number.java

--- Easy/414-Third-Maximum-Number.java
+++ Easy/414-Third-Maximum-Number.java
@@ -1,12 +1,44 @@
 /*
 414. Third Maximum Number
+*/
+/*
+Three marker + check duplicate
+tips: using Integer and initialization = null
 
+Time: O(n)
+Space: O(1)
+
+fail:[1,-2147483648,2]
+*/
+class Solution {
+    public int thirdMax(int[] nums) {
+        Integer first = null, second = null, third = null;
+        for(Integer num : nums){//should be Integer
+            if(num.equals(first) || num.equals(second) || num.equals(third)) continue;//should compare this way
+            
+            if(first == null || num > first){
+                third = second;
+                second = first;
+                first = num;
+            }else if(second == null || (num > second && num < first)){
+                third = second;
+                second = num;
+            }else if(third == null || (num > third && num < second)){
+                third = num;
+            } 
+        }
+        return third == null ? first:third;
+    }
+}
+
+/*
 Three marker + check duplicate
 Time: O(n)
 Space: O(1)
 
 fail:[1,-2147483648,2]
 */
+
 class Solution {
     public int thirdMax(int[] nums) {
         int first = Integer.MIN_VALUE, second = Integer.MIN_VALUE, third = Integer.MIN_VALUE;//对于可能有 Integer.MIN_VALUE 值的情况，不要用这个来初始化
