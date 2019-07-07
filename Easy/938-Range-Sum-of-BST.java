29a63e52baca03867349d68c792c3dd8573d827b
CTCbaxia <ctcbaxia@gmail.com>
2019-07-07 15:16:34 -0700

    Update 938-Range-Sum-of-BST.java

--- Easy/938-Range-Sum-of-BST.java
+++ Easy/938-Range-Sum-of-BST.java
@@ -3,6 +3,27 @@ M
 938. Range Sum of BST
 */
 /*
+DFS + Recursion
+利用 BST 的特质，省略一部分
+
+Time: O(n)
+Space: O(n)
+*/
+class Solution {
+    public int rangeSumBST(TreeNode root, int L, int R) {
+        if(root == null) return 0;
+        if(root.val < L) return rangeSumBST(root.right, L, R);// left branch excluded.
+        else if(root.val > R) return rangeSumBST(root.left, L, R);// right branch excluded.
+        else{
+            return root.val + rangeSumBST(root.left, L, R) + rangeSumBST(root.right, L, R);
+        }
+    }
+}
+
+
+
+
+/*
 DFS + Stack
 
 Time: O(n)
