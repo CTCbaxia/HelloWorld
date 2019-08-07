8aeb74094ad8843f06bd44ae1f9aa82e1f987df4
CTCbaxia <ctcbaxia@gmail.com>
2019-08-07 00:05:18 -0700

    Update

--- Easy/205-Isomorphic-Strings.java
+++ Easy/205-Isomorphic-Strings.java
@@ -23,3 +23,28 @@ class Solution {
         return true;
     }
 }
+/*
+Two pointers + map the char to a same value
+
+The reason we want to use map is to create a relation between key and value.
+mapping a->b is a general way,
+but mapping a->1, b->1 is also a effective way
+
+Time: O(n)
+Space: O(256)
+*/
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        int n = s.length();
+        int[] cs = new int[256];
+        int[] ct = new int[256];
+        for(int i = 0; i < n; i++){
+            char charS = s.charAt(i);
+            char charT = t.charAt(i);
+            if(cs[charS] != ct[charT]) return false;
+            cs[charS] = i + 1;//avoid mapping to 0 when i is 0, mix with something ummaped
+            ct[charT] = i + 1;
+        }
+        return true;
+    }
+}
