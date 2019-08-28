03e6689b6a47a4aae838d274d7bedb4bb6487af8
CTCbaxia <ctcbaxia@gmail.com>
2019-08-28 12:13:16 -0700

    Update 66-Plus-One.java

--- Easy/66-Plus-One.java
+++ Easy/66-Plus-One.java
@@ -1,11 +1,33 @@
 /*
 66. Plus One
-https://leetcode.com/problems/plus-one/description/
-
-TIME: 0706
-RESULT: 100%
-NOTE: easy
 */
+/*
+数学相加的 tricky 版本
+只需要判断是不是 9
+如果需要进一位，那一定是类似于 999...
+所以只需要新建一个首位为 1 的长度 + 1 的数组，其余默认 0
+
+Time: O(n)
+Space: O(1)
+*/
+class Solution {
+    public int[] plusOne(int[] digits) {
+        for(int i = digits.length - 1; i >= 0; i--){
+            if(digits[i] < 9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;//add one for this digit, need to add one for the left one
+        }
+        //if all finished and still need to add one, then must be 9999...
+        int[] nums = new int[digits.length + 1];
+        nums[0] = 1;
+        return nums;
+    }
+}
+
+
+
 /*
 数学相加(carry)
 
