7b58e69b7b5d68ec1b92e33cfd71d6138a77a2b0
CTCbaxia <ctcbaxia@gmail.com>
2019-09-02 22:16:54 -0700

    Update 123-Best-Time-to-Buy-and-Sell-Stock-III.java

--- Hard/123-Best-Time-to-Buy-and-Sell-Stock-III.java
+++ Hard/123-Best-Time-to-Buy-and-Sell-Stock-III.java
@@ -2,9 +2,47 @@
 HARD
 123. Best Time to Buy and Sell Stock III
 
-TIME: 
-RESULT: 
 */
+/*
+Dynamic Programming
+buy[i]: the highest money we can get in buy state for i day 
+(buy state means there is one hold in hand now)
+sell[i]: the highest money we can get in sell state for i day
+(sell state means there is NO hold in hand now)
+
+buy[i] = Math.max(buy[i - 1], sell[i - 1] - prices[i])
+sell[i] = Math.max(sell[i - 1], buy[i - 1] + prices[i])
+
+buy and sell 都有两种状态，每次都更新这四个值
+
+Time: O(n)
+Space: O(1)
+*/
+class Solution {
+    public int maxProfit(int[] prices) {
+        if(prices.length == 0) return 0;
+        int buy1 = Integer.MIN_VALUE, sell1 = 0; 
+        int buy2 = Integer.MIN_VALUE, sell2 = 0;
+        for(int i = 0; i < prices.length; i++){
+            sell2 = Math.max(sell2, buy2 + prices[i]);
+            buy2 = Math.max(buy2, sell1 - prices[i]);
+            sell1 = Math.max(sell1, buy1 + prices[i]);
+            buy1 = Math.max(buy1, -prices[i]);
+            //System.out.println(sell2 + " "+buy2 + " "+sell1+ " "+buy1);
+        }
+        return sell2;
+    }
+}
+
+
+
+
+
+
+
+
+
+
 //没做出来
 class Solution {
     public class point{
