7e07cb0fcdea8ff3743c7958dce522b3a4c24e52
CTCbaxia <ctcbaxia@gmail.com>
2019-09-08 22:16:50 -0400

    Create FacebookFriendRecommendation.java

---
+++ Medium/FacebookFriendRecommendation.java
@@ -0,0 +1,15 @@
+/*
+Facebook Question - My intern video interview
+
+Q1.  Given two “ids” and a function getFriends(id) to get the list of friends of that person id, 
+write a function that returns the list of mutual friends.
+Q2.  Given an “id” and a function getFriends(id) to get the list of friends of that person id, 
+write a function that returns the list of “friends of friends” in the order of decreasing number of mutual friends, 
+as in friend recommendations.
+
+http://www.crazyforcode.com/facebook-interview-set-1/
+https://leetcode.com/discuss/interview-question/125156/fb-082016-phone-interview-find-2nd-degree-connections
+*/
+
+Map<Node, Frequency>
+PriorityQueue or Bucket Sort
