6fc402e4719c1795794713c644c0b3e3d74ee5a2
CTCbaxia <ctcbaxia@gmail.com>
2019-09-11 23:11:56 -0400

    Update 523-Continuous-Subarray-Sum.java

--- Medium/523-Continuous-Subarray-Sum.java
+++ Medium/523-Continuous-Subarray-Sum.java
@@ -8,7 +8,42 @@ RESULT: 82% - 7ms
 NOTES: 
 1. % 除数不能为 0 
 2. % 相加减的关系：模相等，则他们的差一定可以整除 k
+3. 一个负数 num % k，结果为负数的摩，这个时候加上一个 k 就是正数结果了
 */
+/*
+Map<Integer, Integer> : <presum, index>
+**note:
+cannot mode 0
+这个题 k 是可以等于负数的，k 等于负数摩的结果和k为正一样，且也成立
+
+**tips: if we have negative value, 
+**still mod k, and for res = num % k, if res < 0, res += k
+**do the same thing
+
+Time: O(n)
+Space: O(k)
+*/
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int presum = 0;
+        map.put(presum, -1);
+        for(int i = 0; i < nums.length; i++){
+            presum += nums[i];
+            if(k != 0) presum %= k;//cannot mod 0
+            if(map.containsKey(presum)){
+                if(i - map.get(presum) >= 2) return true;
+            }else{
+                //only add when there is no presum before, otherwise you will make the index bigger
+                map.put(presum, i);
+            }
+        }
+        return false;
+    }
+}
+
+
+
 /*
 preSum + mod
 
