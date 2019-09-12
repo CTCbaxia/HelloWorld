eb26b13d809051f781f079463f8d2c98ebe89507
CTCbaxia <ctcbaxia@gmail.com>
2019-09-11 23:12:01 -0400

    Update 50-Pow(x, n).java

--- Medium/50-Pow(x, n).java
+++ Medium/50-Pow(x, n).java
@@ -6,6 +6,105 @@ https://leetcode.com/problems/powx-n/description/
 TIME: 0902 - 20min
 RESULT: 39% - 13ms
 */
+/*
+Recursion
+
+**tips:
+任何数的 0 次方都等于 0
+
+Time: O(logn)
+Space: O(logn)
+*/
+class Solution {
+    public double myPow(double x, int n) {
+        // if(x == 0) return x;
+        if(n < 0){
+            return 1/x * myPow(1/x, -(n + 1));//for Integer.MIN_VALUE;
+        } 
+        if(n == 0) return 1;
+        
+        //here n >= 0
+        if(n % 2 == 0){
+            double half = myPow(x, n >> 1);
+            return half * half;
+        }else{
+            return x * myPow(x, n - 1);
+        }
+    }
+}
+
+
+/*
+Iteration
+
+**tips:
+任何数的 0 次方都等于 0
+
+Time: O(logn)
+Space: O(logn)
+*/
+class Solution {
+    public double myPow(double x, int n) {
+        if(n == 0) return 1;
+        double res = 1;
+        if(n < 0){
+            x = 1/x;
+            n = -(n + 1);
+            res *= x;//current x 分数
+        } 
+        //x >= 0, n > 0
+        while(n > 0){
+            //here n >= 0
+            if(n % 2 == 0){
+                x = x * x;
+                n = n >> 1;
+            }else{
+                res *= x;//will always arrive here
+                n = n - 1;
+            }
+        }
+        return res;
+    }
+}
+
+
+
+/*
+FB 变种：If we cannot use * or /
+works only for x >= 0 and n >= 0
+
+For example to calculate 5^6.
+1) First 5 times add 5, we get 25. (5^2)
+2) Then 5 times add 25, we get 125. (5^3)
+3) Then 5 time add 125, we get 625 (5^4)
+4) Then 5 times add 625, we get 3125 (5^5)
+5) Then 5 times add 3125, we get 15625 (5^6)
+
+**tips:
+任何数的 0 次方都等于 0
+
+Time: O(logn)
+Space: O(logn)
+*/
+class Solution {
+    public double myPow(double x, int n) {
+        //for x >= 0 and n >= 0
+        if(n == 0) return 1;
+        double answer = x, increment = x;
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j < x; j++){
+                answer += increment;
+            }
+            increment = answer;
+        }
+        return answer;
+    }
+}
+
+
+
+//Internship =======================================
+
 class Solution {
     public double myPow(double x, int n) {
         if(n == 0) return 1;
