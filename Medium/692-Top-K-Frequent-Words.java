ee88dfd1c1b06fed7e562665380190540e0a896d
CTCbaxia <ctcbaxia@gmail.com>
2019-09-13 23:10:50 -0400

    Update 692-Top-K-Frequent-Words.java

--- Medium/692-Top-K-Frequent-Words.java
+++ Medium/692-Top-K-Frequent-Words.java
@@ -10,6 +10,90 @@ NOTES:
 2. how to sort collections: https://stackoverflow.com/questions/2477261/how-to-sort-a-collectiont
 */
 /*
+Map + Bucket Sort
+
+Time: O(n)
+Space: O(n)
+*/
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        List<String>[] bucket = new ArrayList[words.length + 1];
+        Map<String, Integer> map = new HashMap<>();
+        //build map
+        for(String s : words){
+            map.put(s, map.getOrDefault(s, 0) + 1);
+        }
+        //put bucket
+        for(Map.Entry<String, Integer> entry : map.entrySet()){
+            int f = entry.getValue();
+            if(bucket[f] == null) bucket[f] = new ArrayList<>();
+            bucket[f].add(entry.getKey());
+        }
+        //add res
+        List<String> res = new ArrayList<>();
+        for(int f = words.length; f >= 0; f--){
+            if(bucket[f] == null) continue;
+            List<String> w = bucket[f];
+            Collections.sort(w);
+            for(int i = 0; i < w.size() && k > 0; i++, k--){
+                res.add(w.get(i));
+            }
+        }
+        return res;
+    }
+}
+/*
+Map + PQ
+Time: O(nlogk)
+Space: O(n)
+*/
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        Map<String, Integer> map = new HashMap<>();
+        //build map
+        for(String s : words){
+            map.put(s, map.getOrDefault(s, 0) + 1);
+        }
+        //put PQ, frequent ascending, string descending
+        PriorityQueue<String> pq = new PriorityQueue<String>(new Comparator<String>(){
+            public int compare(String s1, String s2){
+                if(map.get(s1) != map.get(s2)) return map.get(s1) - map.get(s2);
+                else return s2.compareTo(s1);// > 0 则 s2 > s1
+            }
+        });
+        for(Map.Entry<String, Integer> entry : map.entrySet()){
+            pq.offer(entry.getKey());
+            if(pq.size() > k) pq.poll();
+        }
+        List<String> res = new ArrayList<>();
+        while(!pq.isEmpty()){
+            res.add(pq.poll());
+        }
+        Collections.reverse(res);//当前的答案是反的
+        return res;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 PriorityQueue
 
 Time: O(n) + O(nlogk) (worst case #key = n)
@@ -44,7 +128,7 @@ class Solution {
         }
         List<String> res = new ArrayList<>();
         while(!pq.isEmpty()){
-            res.add(0, pq.poll().word);
+            res.add(0, pq.poll().word);//这个操作是 O(n^2)
         }
         return res;
     }
