533b4196354cc203ed072a1ce90bd93bd9e5097a
CTCbaxia <ctcbaxia@gmail.com>
2019-09-13 23:10:57 -0400

    Update 199-Binary-Tree-Right-Side-View.java

--- Medium/199-Binary-Tree-Right-Side-View.java
+++ Medium/199-Binary-Tree-Right-Side-View.java
@@ -10,6 +10,60 @@ METHOD:
 2. BFS
 */
 /*
+DFS : using level to compare res.size()
+
+Time: O(n)
+Space: O(n) - worst case the height is n, but general it is O(logn)
+*/
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if(root == null) return res;
+        dfs(root, res, 0);
+        return res;
+    }
+    private void dfs(TreeNode node, List<Integer> res, int level){//don't have null
+        if(res.size() == level) res.add(node.val);
+        
+        if(node.right != null) dfs(node.right, res, level + 1);
+        if(node.left != null) dfs(node.left, res, level + 1);
+    }
+}
+
+/*
+BFS : Level Order Traverse the tree
+Time: O(n)
+Space: O(n)
+*/
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if(root == null) return res;
+        
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i = 0; i < size; i++){
+                TreeNode node = q.poll();
+                if(i == 0) res.add(node.val);
+                if(node.right != null) q.offer(node.right);//从右往左放
+                if(node.left != null) q.offer(node.left);
+            }
+        }
+        return res;
+    }
+}
+
+
+
+
+
+
+
+
+
+/*
 DFS + level vs list size
 dfs 先右后左，记录 level
 如果这个 level 在 result 里面还没有，说明他是第一个出现在这个 level 的
