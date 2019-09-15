e0034b6a2b739cc74eb868919bd5ffd1bdb2ec8a
CTCbaxia <ctcbaxia@gmail.com>
2019-09-15 14:39:39 -0400

    Update 674-Longest-Continuous-Increasing-Subsequence.java

--- Easy/674-Longest-Continuous-Increasing-Subsequence.java
+++ Easy/674-Longest-Continuous-Increasing-Subsequence.java
@@ -7,6 +7,31 @@ NOTES:
 这种题肯定要遍历完才会知道最大的
 */
 /*
+Count++ or set 0
+
+Time: O(n)
+Space: O(1)
+*/
+class Solution {
+    public int findLengthOfLCIS(int[] nums) {
+        int res = 0, len = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(i == 0 || i > 0 && nums[i] > nums[i - 1]){
+                len++;
+            }else{
+                res = Math.max(res, len);
+                len = 1;
+            }
+        }
+        res = Math.max(res, len);//要注意end
+        return res;
+    }
+}
+
+
+
+
+/*
 每次更新最长距离，如果遇到 drop 就把 len 清零
 Time: O(n)
 Space: O(1)
