54ec2f6e0fccf25eff10b0efb605a6d6116848e1
CTCbaxia <ctcbaxia@gmail.com>
2019-09-14 23:57:41 -0400

    Update 200-Number-of-Islands.java

--- Medium/200-Number-of-Islands.java
+++ Medium/200-Number-of-Islands.java
@@ -6,6 +6,43 @@ TIME: 0706
 RESULT: 100%
 
 */
+/*
+DFS + mark inplace
+Time: O(mn)
+Space: O(1)
+*/
+class Solution {
+    public int numIslands(char[][] grid) {
+        int res = 0;
+        // if(grid.length == 0 || grid[0].length == 0) return res;
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
+                if(grid[i][j] == '1'){
+                    res++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return res;
+    }
+    private void dfs(char[][] grid, int i, int j){
+        int[][] directions = {{0,1},{0,-1},{1,0},{-1,0}};
+        grid[i][j] = '0';
+        for(int[] dir : directions){
+            int x = i + dir[0];
+            int y = j + dir[1];
+            if(x >= 0 && y >= 0 && x < grid.length && y < grid[0].length && grid[x][y] == '1'){
+                dfs(grid, x, y);
+            }
+        }
+    }
+}
+
+
+
+
+
+
 // modify the input
 class Solution {
     public int numIslands(char[][] grid) {
