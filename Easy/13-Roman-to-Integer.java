02100a37c2f90cc5a96cefd5ce1536d5bf40f208
CTCbaxia <ctcbaxia@gmail.com>
2019-09-15 21:04:25 -0400

    Update 13-Roman-to-Integer.java

--- Easy/13-Roman-to-Integer.java
+++ Easy/13-Roman-to-Integer.java
@@ -7,6 +7,41 @@ RESULT:
 NOTES:
 */
 /*
+Iteration
+build unit<->val, if the next char is larger than the curent char, make -val
+
+Time: O(n) , n = s.length
+Space: O(1)
+*/
+class Solution {
+    public int romanToInt(String s) {
+        //build map
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        
+        //compute
+        int res = 0;
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(i + 1< s.length() && map.get(s.charAt(i + 1)) > map.get(c)) res -= map.get(c);
+            else res += map.get(c);
+        }
+        return res;
+    }
+}
+
+
+
+
+
+
+/*
 找到对应数字
 根据相对位置计算
 
