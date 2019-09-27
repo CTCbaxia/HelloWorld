ea20fe392217077f6d5af40a460b94f08d77c891
CTCbaxia <ctcbaxia@gmail.com>
2019-09-26 20:57:10 -0400

    Update 843-Guess-the-Word.java

--- Hard/843-Guess-the-Word.java
+++ Hard/843-Guess-the-Word.java
@@ -2,6 +2,12 @@
 HARD
 843. Guess the Word
 
+Onsite: 限制条件比LC 843多一些，
+长度是５，都是uppercase, 没有重复字母。
+先实现guess function, 自己设计数据结构，返回有几个字母match（不需要位置也一样，存在于secret word里就算）和是否猜中。
+这个就是基础的字符串比较，主要是注意对word进行validation。
+然后就是问怎么猜了。就大概说了LC 843里的方法，preprocessing dictionary,　然后根据猜的结果缩小范围。代码没时间写完。
+
 */
 /*
 Random Guess + BFS
@@ -52,6 +58,24 @@ class Solution {
 
 
 /*
+MinMax: + BFS
+同样的BFS缩小范围的方法，但是我们希望能每次都最大化的缩小范围。
+所以每次选取 family 最大的 word 来进行测试
+
+Anyone who doesn't know why checking 0 match instead of 1,2,3...6 matches, please take a look at this comment. 
+The probability of two words with 0 match is (25/26)^6 = 80%. 
+That is to say, for a candidate word, 
+we have 80% chance to see 0 match with the secret word. 
+In this case, we had 80% chance to eliminate the candidate word and its "family" words which have at least 1 match. 
+Additionally, in order to delete a max part of words, we select a candidate who has a big "family" (fewest 0 match with other words).
+
+我们希望找有最大家族的，因为这样有更大可能这个 word 会和 secret 有匹配。
+不然很有可能这个 word 压根就跟 secret 不匹配，那么我们其实根本就删不了什么words
+
+Time: O(n^2)
+Space: O(n)
+*/
+/*
 MinMax:
 同样的BFS缩小范围的方法，但是我们希望能每次都最大化的缩小范围。
 所以每次选取 family 最大的 word 来进行测试
@@ -87,9 +111,12 @@ class Solution {
                 }
             }
             String word = "";
-            int max = Integer.MAX_VALUE;
+            int min = Integer.MAX_VALUE;
             for(Map.Entry<String, Integer> entry : count.entrySet()){
-                if(entry.getValue() < max) word = entry.getKey();
+                if(entry.getValue() < min){
+                    min = entry.getValue();
+                    word = entry.getKey();
+                } 
             }
             sameNum = master.guess(word);
             
@@ -111,57 +138,14 @@ class Solution {
         return count;
     }
 }
-/*
-using pair
-*/
-class Solution {
-    public void findSecretWord(String[] wordlist, Master master) {
-        List<String> wl = Arrays.asList(wordlist);
-        int sameNum = 0;
-        int tries = 0;
-        while(sameNum < 6 && tries++ < 10){
-            //get minmax
-            Map<String, Integer> count = new HashMap<>();
-            for(String s : wl) count.put(s, 0);
-            
-            for(int i = 0; i < wl.size(); i++){
-                for(int j = i + 1; j < wl.size(); j++){
-                    String w1 = wl.get(i);
-                    String w2 = wl.get(j);
-                    if(sameLetter(w1, w2) == 0){
-                        count.put(w1, count.get(w1) + 1);
-                        count.put(w2, count.get(w2) + 1);
-                    }
-                }
-            }
-            Pair<String, Integer> pending = new Pair<>("", Integer.MAX_VALUE);
-            
-            for(String s : wl){
-                if(count.get(s) < pending.getValue()){
-                    pending = new Pair<>(s, count.get(s));
-                }
-            }
-            String word = pending.getKey();
-            sameNum = master.guess(word);
-            
-            List<String> wl2 = new ArrayList<>();
-            for(String s : wl){
-                if(sameLetter(s, word) == sameNum){
-                    wl2.add(s);
-                }
-            }
-            wl = wl2;
-        }
-        return;
-    }
-    private int sameLetter(String s, String t){
-        int count = 0;
-        for(int i = 0; i < s.length(); i++){
-            if(s.charAt(i) == t.charAt(i)) count++;
-        }
-        return count;
-    }
-}
+
+
+
+
+
+
+
+
 
 
 
