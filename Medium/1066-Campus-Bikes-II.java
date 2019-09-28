58d2d9de23a2aa677e51daa3ff64aa3bb040b52c
CTCbaxia <ctcbaxia@gmail.com>
2019-09-27 23:02:39 -0400

    Update 1066-Campus-Bikes-II.java

--- Medium/1066-Campus-Bikes-II.java
+++ Medium/1066-Campus-Bikes-II.java
@@ -9,7 +9,7 @@ Since 1 <= M, N <= 10, it can be acceptable
 How to prun?
 We only want the minimum value, so just stop when getting a value larger than the current min
 
-Time: O(M^N) + pruning may make it faster
+Time: O(numBikes * (numBikes - 1) * (numBikes - 2).... (numBikes - numWorkers)) + pruning may make it faster
 Space: O(N)//stack 深度
 */
 class Solution {
@@ -67,5 +67,42 @@ class Solution {
     }
     private int computeDis(int[] worker, int[] bike){
         return Math.abs(worker[0] - bike[0]) + Math.abs(worker[1] - bike[1]);
+    }
+}
+
+
+
+
+//dp + bitmask state
+//看这个答案：https://leetcode.com/problems/campus-bikes-ii/discuss/305218/DFS-%2B-Pruning-And-DP-Solution
+class Solution{
+     public int assignBikes(int[][] workers, int[][] bikes) {
+        int n = workers.length;
+        int m = bikes.length;
+        int[][] dp = new int[n + 1][1 << m];
+        for (int[] d : dp) {
+            Arrays.fill(d, Integer.MAX_VALUE / 2);
+        }
+        dp[0][0] = 0;
+        int min = Integer.MAX_VALUE;
+        for (int i = 1; i <= n; i++) {//这个 i 对应 worker 是要大 1 的
+            for (int s = 1; s < (1 << m); s++) {
+                for (int j = 0; j < m; j++) {
+                    if ((s & (1 << j)) == 0) {//如果在这个 state 里面，自行车 j 没有被使用，skip
+                        continue;
+                    }
+                    int prev = s ^ (1 << j);//找到在未使用这个 j 自行车时候的 prev state
+                    dp[i][s] = Math.min(dp[i - 1][prev] + dis(workers[i - 1], bikes[j]), dp[i][s]) ;
+                    if (i == n) {//最后一个人了，也就是 s 这里表示全部分配完了
+                        min = Math.min(min, dp[i][s]);
+                    }
+                }
+            }
+        }
+        return min;
+    }
+  
+    public int dis(int[] p1, int[] p2) {
+        return Math.abs(p1[0] - p2[0]) + Math.abs(p1[1] - p2[1]);
     }
 }
