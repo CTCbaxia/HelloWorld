6dd74b855a74f238d28bb1e2adc2b1d497850918
CTCbaxia <ctcbaxia@gmail.com>
2019-09-27 23:02:37 -0400

    Update 685-Redundant-Connection-II.java

--- Hard/685-Redundant-Connection-II.java
+++ Hard/685-Redundant-Connection-II.java
@@ -3,6 +3,94 @@ H
 685. Redundant Connection II
 */
 /*
+Union Find
+case1: 2 parents no circle
+case2: 2 parents with circle
+case3: 1 parent with circle
+
+       1
+      / \
+     *   *
+    2 * - 3
+   /       \
+  *         *
+ 4          5
+
+
+       1 *---
+      / \   |
+     *   *  |
+    2     3 |
+   /       \|
+  *         *
+ 4          5
+
+           1
+          / \
+         *   *
+  ----* 2     3
+  |    /       \
+  |   *         *
+  |  4          5
+  | /
+   *
+  6
+
+1. see if there is multi parent, remove one parent and candidate parents edge
+2. check if there is circle
+
+Time: O(n)
+Space: O(n)
+*/
+class Solution {
+    public int[] findRedundantDirectedConnection(int[][] edges) {
+        int n = edges.length;
+        
+        //近亲 union find
+        int[] parent = new int[n + 1];
+        int[] cand1 = null, cand2 = null;
+        for(int[] e : edges){
+            if(parent[e[1]] != 0){//e[1] already has one parent
+                cand1 = new int[]{parent[e[1]], e[1]};
+                cand2 = new int[]{e[0], e[1]};//try remove cand2 because it shows later
+                e[0] = -1;//make this cand2 invalid to test
+                break;
+            }else{
+                parent[e[1]] = e[0];
+            }
+        }
+        //reset parent
+        for(int i = 1; i <= n; i++) parent[i] = i;
+
+        //正式 union find to find if there is a circle
+        for(int[] e : edges){
+            if(e[0] == -1) continue;//this edge is the removed cand2
+            int p1 = find(e[0], parent);
+            int p2 = find(e[1], parent);
+            if(p1 == p2){//find circle
+                if(cand2 == null) return e;//no multi parent, return current edge
+                else return cand1;//has multi parent, previously removed the wrong one
+            }else{
+                parent[p2] = p1;
+            }
+        }
+        return cand2;
+        
+    }
+    private int find(int n, int[] parent){
+        if(parent[n] == n) return n;
+        parent[n] = find(parent[n], parent);
+        return parent[n];
+    }
+
+}
+
+
+
+
+
+
+/*
 Union Find + Find Circle
 2 invalid cases
 1) one node has two parents --> remove one candidate edge, try union find to see if there is only one group
